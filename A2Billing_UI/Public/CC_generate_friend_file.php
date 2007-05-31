70c20a54edde225a198907f201d54a02c7b6dfa2
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-05-31 14:56:11 +0000

    BUG : BLANK screen if try to generate sip/iax friends and if empty
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@18 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- A2Billing_UI/Public/CC_generate_friend_file.php
+++ A2Billing_UI/Public/CC_generate_friend_file.php
@@ -13,9 +13,9 @@ getpost_ifset(array('action', 'atmenu' ));
 
 
 if (! has_rights (ACX_CUSTOMER)){ 
-	   Header ("HTTP/1.0 401 Unauthorized");
-	   Header ("Location: PP_error.php?c=accessdenied");	   
-	   die();	   
+	Header ("HTTP/1.0 401 Unauthorized");
+	Header ("Location: PP_error.php?c=accessdenied");	   
+	die();
 }
 
 
@@ -70,129 +70,99 @@ restrictcid, rtptimeout, rtpholdtimeout, musiconhold, regseconds, ipaddr, cancal
 	$instance_table_friend = new Table($TABLE_BUDDY,'id, '.$FG_QUERY_EDITION);	
 	$list_friend = $instance_table_friend -> Get_list ($DBHandle, '', null, null, null, null);
 	
-	if (!is_array($list_friend) || count($list_friend)==0) exit();
-	
-		
-	error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
-	$fd=fopen($buddyfile,"w");
-	if (!$fd){   
-		$error_msg= "</br><center><b><font color=red>Could not open buddy file '$buddyfile'</font></b></center>";		
+	if (!is_array($list_friend) || count($list_friend)==0){ 
+		$error_msg= "</br><center><b><font color=red>".gettext("There is no ").$atmenu." ! </font></b></center>";
 	}else{
-		foreach ($list_friend as $data){
-			$line="\n\n[".$data[1]."]\n";
-			if (fwrite($fd, $line) === FALSE) {  
-				echo "Impossible to write to the file ($$buddyfile)";
-				exit;
-			}
 		
-			for ($i=1;$i<count($data)-1;$i++){
-				if (strlen($data[$i+1])>0){
-					if (trim($list_names[$i]) == 'allow'){
-						$codecs = explode(",",$data[$i+1]);
-						$line = "";
-						foreach ($codecs as $value)
-							$line .= trim($list_names[$i]).'='.$value."\n";
-					}else    $line = (trim($list_names[$i]).'='.$data[$i+1]."\n");
-					if (fwrite($fd, $line) === FALSE){
-						echo "Impossible to write to the file ($buddyfile)";
-						break;
+		error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+		$fd=fopen($buddyfile,"w");
+		if (!$fd){   
+			$error_msg= "</br><center><b><font color=red>".gettext("Could not open buddy file :")." '$buddyfile'</font></b></center>";
+		}else{
+			foreach ($list_friend as $data){
+				$line="\n\n[".$data[1]."]\n";
+				if (fwrite($fd, $line) === FALSE) {  
+					$error_msg = gettext("Impossible to write to the file")." : $buddyfile";
+					break;
+				}
+				
+				for ($i=1;$i<count($data)-1;$i++){
+					if (strlen($data[$i+1])>0){
+						if (trim($list_names[$i]) == 'allow'){
+							$codecs = explode(",",$data[$i+1]);
+							$line = "";
+							foreach ($codecs as $value)
+								$line .= trim($list_names[$i]).'='.$value."\n";
+						}else    $line = (trim($list_names[$i]).'='.$data[$i+1]."\n");
+						if (fwrite($fd, $line) === FALSE){
+							$error_msg = gettext("Impossible to write to the file")." : $buddyfile";
+							break 2;
+						}
 					}
 				}
 			}
+			fclose($fd);
 		}
-		fclose($fd);
 	}
 }
+
+
+$smarty->display('main.tpl');
+
+
+echo $CC_help_sipfriend_reload;
+
 ?>
 
+<table width="60%" border="0" align="center" cellpadding="0" cellspacing="0" >
+
+<TR> 
+  <TD style="border-bottom: medium dotted #555555">&nbsp; </TD>
+</TR>
+<tr><FORM NAME="sipfriend">
+	<td height="31" class="bgcolor_001" style="padding-left: 5px; padding-right: 3px;" align=center>
+	<br><br>
+	<b>
+	<?php 	
+		if (strlen($error_msg)>0){			
+			echo $error_msg;
+		}elseif ( $action != "reload" ){		
+			if ( $atmenu == "sipfriend" ){
+				echo gettext("The sipfriend file has been generated : ").$buddyfile;
+			}else{
+				echo gettext("The iaxfriend file has been generated : ").$buddyfile;
+			}
+		
+	?>
+	
+	
+	<br><br><br>
+	<a href="<?php  echo $PHP_SELF."?atmenu=$atmenu&action=reload";?>"><img src="<?php echo Images_Path;?>/icon_refresh.gif" /> 
+		<?php echo gettext("Click here to reload Asterisk Server"); ?>
+	</a>
+	
+	<?php 
+		}else{
+			
+			echo gettext("Asterisk has been reload!");
+		
+		}
+	?>
+	<br><br><br>
+	
+	</b>
+	  </td></FORM>
+  </tr>
+</table>
+<br><br>
+
+
+
+<br>
 
 
 <?php
-	$smarty->display('main.tpl');
-?>
-<script language="JavaScript" type="text/JavaScript">
-<!--
-function MM_openBrWindow(theURL,winName,features) { //v2.0
-  window.open(theURL,winName,features);
-}
 
-function openURL(theLINK)
-{
-      // grab index number of the selected option
-      selInd = document.theForm.choose_list.selectedIndex;
-      // get value of the selected option
-      goURL = document.theForm.choose_list.options[selInd].value;
-      // redirect browser to the grabbed value (hopefully a URL)	  
-     self.location.href = theLINK + goURL;
-}
+$smarty->display('footer.tpl');
 
-function openURLFilter(theLINK)
-{
-      selInd = document.theFormFilter.choose_list.selectedIndex;
-	  if(selInd==0){return false;}
-      goURL = document.theFormFilter.choose_list.options[selInd].value;      
-      this.location.href = theLINK + goURL;	  
-}
-
-//-->
-</script>
-
-           
-	  
-<?php
-	echo $CC_help_sipfriend_reload;
-?>
-
-	    
-	 
-	  
-	  <table width="<?php echo $FG_HTML_TABLE_WIDTH?>" border="0" align="center" cellpadding="0" cellspacing="0" >
-	  
-		<TR> 
-          <TD style="border-bottom: medium dotted #252525">&nbsp; </TD>
-        </TR>
-		<tr><FORM NAME="sipfriend">
-            <td height="31" class="bgcolor_001" style="padding-left: 5px; padding-right: 3px;" align=center>
-			<br><br>
-			<b>
-			<?php 	
-				if (strlen($error_msg)>0){			
-					echo $error_msg;
-				}elseif ( $action != "reload" ){		
-					if ( $atmenu == "sipfriend" ){
-						echo gettext("The sipfriend file has been generated : ").$buddyfile;
-					}else{
-						echo gettext("The iaxfriend file has been generated : ").$buddyfile;
-					}
-				
-			?>
-			
-			
-			<br><br><br>
-			<a href="<?php  echo $PHP_SELF."?atmenu=$atmenu&action=reload";?>"><img src="<?php echo Images_Path;?>/icon_refresh.gif" /> 
-				<?php echo gettext("Click here to reload Asterisk Server"); ?>
-			</a>
-			
-			<?php 
-				}else{
-					
-					echo gettext("Asterisk has been reload!");
-				
-				}
-			?>
-			<br><br><br>
-			
-			</b>
-			  </td></FORM>
-          </tr>
-	   </table>
-	  <br><br>
-	  
-	 
-	  
-	  <br>
-	 
-	 
-<?php
-	$smarty->display('footer.tpl');
 ?>
