8c874710ed3995dffbc0bcc38a5f1f754c7ca578
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-06-14 23:01:20 +0000

    BUG : Qualify 7 char iax & sip friends
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@35 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/mysql-5.x/a2billing-mysql-schema-MYSQL.5.X-v1.3.0.sql
+++ DataBase/mysql-5.x/a2billing-mysql-schema-MYSQL.5.X-v1.3.0.sql
@@ -506,7 +506,7 @@ CREATE TABLE cc_iax_buddies (
     mask 									CHAR(95),
     pickupgroup 							CHAR(10),
     port 									CHAR(5) DEFAULT '' NOT NULL,
-    qualify 								CHAR(3) DEFAULT 'yes',
+    qualify 								CHAR(7) DEFAULT 'yes',
     restrictcid 							CHAR(1),
     rtptimeout 								CHAR(3),
     rtpholdtimeout 							CHAR(3),
