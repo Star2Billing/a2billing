c7ce3d4959d935eb24514814e023672884911a40
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-08-03 15:03:47 +0000

    FEATURE : CSV & XML Export on Call-report
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@123 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- A2Billing_UI/Public/call-log-customers.php
+++ A2Billing_UI/Public/call-log-customers.php
@@ -35,11 +35,10 @@ if (($_GET[download]=="file") && $_GET[file] )
 	header("Expires: 0");
 	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 	header("Content-transfer-encoding: binary");
-			
+	
 	@readfile($dl_full);
 	exit();
 }
-
 
 
 if (!isset ($current_page) || ($current_page == "")){	
@@ -77,64 +76,37 @@ $list_calltype = array(); 	$list_calltype["0"]  = array( "STANDARD", "0");	 $lis
 $list_calltype["2"]  = array( "DIDCALL", "2"); $list_calltype["3"]  = array( "DID_VOIP", "3"); $list_calltype["4"]  = array( "CALLBACK", "4");
 $list_calltype["5"]  = array( "PREDICT", "5");
 
-//$link = DbConnect();
+
 $DBHandle  = DbConnect();
 
-// The variable Var_col would define the col that we want show in your table
-// First Name of the column in the html page, second name of the field
 $FG_TABLE_COL = array();
 
-
-/*******
-Calldate Clid Src Dst Dcontext Channel Dstchannel Lastapp Lastdata Duration Billsec Disposition Amaflags Accountcode Uniqueid Serverid
-*******/
-
 $FG_TABLE_COL[]=array (gettext("Calldate"), "starttime", "15%", "center", "SORT", "19", "", "", "", "", "", "display_dateformat");
-//$FG_TABLE_COL[]=array ("Callend", "stoptime", "15%", "center", "SORT", "19");
-
-
 $FG_TABLE_COL[]=array (gettext("Source"), "src", "7%", "center", "SORT", "30");
-
 $FG_TABLE_COL[]=array (gettext("CalledNumber"), "calledstation", "10%", "center", "SORT", "30", "", "", "", "", "", "remove_prefix");
 $FG_TABLE_COL[]=array (gettext("Destination"), "destination", "10%", "center", "SORT", "30", "", "", "", "", "", "remove_prefix");
-//$FG_TABLE_COL[]=array ("Country",  "calledcountry", "10%", "center", "SORT", "30", "lie", "country", "countryname", "countrycode='%id'", "%1");
-//$FG_TABLE_COL[]=array ("Site", "site_id", "7%", "center", "sort", "15", "lie", "site", "name", "id='%id'", "%1");
-
 $FG_TABLE_COL[]=array (gettext("Duration"), "sessiontime", "6%", "center", "SORT", "30", "", "", "", "", "", "display_minute");
-
 $FG_TABLE_COL[]=array (gettext("CardUsed"), "username", "6%", "center", "SORT", "30");
 $FG_TABLE_COL[]=array (gettext("Trunk"), "trunkcode", "6%", "center", "SORT", "30");
 $FG_TABLE_COL[]=array ('<acronym title="'.gettext("Terminate Cause").'">'.gettext("TC").'</acronym>', "terminatecause", "7%", "center", "SORT", "30");
 $FG_TABLE_COL[]=array (gettext("Calltype"), "sipiax", "6%", "center", "SORT",  "", "list", $list_calltype);
-//$FG_TABLE_COL[]=array ("DestID", "destID", "12%", "center", "SORT", "30");
-
-//if ($_SESSION["is_admin"]==1) $FG_TABLE_COL[]=array ("Con_charg", "connectcharge", "12%", "center", "SORT", "30");
-//if ($_SESSION["is_admin"]==1) $FG_TABLE_COL[]=array ("Dis_charg", "disconnectcharge", "12%", "center", "SORT", "30");
-//if ($_SESSION["is_admin"]==1) $FG_TABLE_COL[]=array ("Sec/mn", "secpermin", "12%", "center", "SORT", "30");
-
-
-//if ($_SESSION["is_admin"]==1) $FG_TABLE_COL[]=array ("Buycosts", "buycosts", "12%", "center", "SORT", "30");
-//$FG_TABLE_COL[]=array (gettext("InitialRate"), "calledrate", "7%", "center", "SORT", "30", "", "", "", "", "", "display_2dec");
 $FG_TABLE_COL[]=array (gettext("Buy"), "buycost", "8%", "center", "SORT", "30", "", "", "", "", "", "display_2bill");
 $FG_TABLE_COL[]=array (gettext("Sell"), "sessionbill", "8%", "center", "SORT", "30", "", "", "", "", "", "display_2bill");
 $FG_TABLE_COL[]=array (gettext("Margin"), "margin", "7%", "center", "SORT", "30", "", "", "", "", "", "display_2dec_percentage");
 $FG_TABLE_COL[]=array (gettext("Markup"), "markup", "7%", "center", "SORT", "30", "", "", "", "", "", "display_2dec_percentage");
 
-if (LINK_AUDIO_FILE)
+if (LINK_AUDIO_FILE){
 	$FG_TABLE_COL[]=array ("", "uniqueid", "1%", "center", "", "30", "", "", "", "", "", "linkonmonitorfile");
+}
 
-
-
-// ??? cardID
 $FG_TABLE_DEFAULT_ORDER = "t1.starttime";
 $FG_TABLE_DEFAULT_SENS = "DESC";
 	
 // This Variable store the argument for the SQL query
-// t1.calledrate,
 $FG_COL_QUERY='t1.starttime, t1.src, t1.calledstation, t1.destination, t1.sessiontime, t1.username, t3.trunkcode, t1.terminatecause, t1.sipiax, t1.buycost, t1.sessionbill, case when t1.sessionbill!=0 then ((t1.sessionbill-t1.buycost)/t1.sessionbill)*100 else NULL end as margin,case when t1.buycost!=0 then ((t1.sessionbill-t1.buycost)/t1.buycost)*100 else NULL end as markup';
-// t1.stoptime,
-if (LINK_AUDIO_FILE) 
+if (LINK_AUDIO_FILE) {
 	$FG_COL_QUERY .= ', t1.uniqueid';
+}
 
 $FG_COL_QUERY_GRAPH='t1.callstart, t1.duration';
 
@@ -159,10 +131,9 @@ $FG_HTML_TABLE_WIDTH="98%";
 
 
 
-
-	if ($FG_DEBUG == 3) echo "<br>Table : $FG_TABLE_NAME  	- 	Col_query : $FG_COL_QUERY";
-	$instance_table = new Table($FG_TABLE_NAME, $FG_COL_QUERY);
-	$instance_table_graph = new Table($FG_TABLE_NAME, $FG_COL_QUERY_GRAPH);
+if ($FG_DEBUG == 3) echo "<br>Table : $FG_TABLE_NAME  	- 	Col_query : $FG_COL_QUERY";
+$instance_table = new Table($FG_TABLE_NAME, $FG_COL_QUERY);
+$instance_table_graph = new Table($FG_TABLE_NAME, $FG_COL_QUERY_GRAPH);
 
 
 if ( is_null ($order) || is_null($sens) ){
@@ -171,43 +142,39 @@ if ( is_null ($order) || is_null($sens) ){
 }
 
 if ($posted==1){
-  
-  function do_field($sql,$fld,$dbfld){
-  		$fldtype = $fld.'type';
+	function do_field($sql,$fld,$dbfld){
+		$fldtype = $fld.'type';
 		global $$fld;
 		global $$fldtype;		
-        if ($$fld){
-                if (strpos($sql,'WHERE') > 0){
-                        $sql = "$sql AND ";
-                }else{
-                        $sql = "$sql WHERE ";
-                }
+		if ($$fld){
+				if (strpos($sql,'WHERE') > 0){
+						$sql = "$sql AND ";
+				}else{
+						$sql = "$sql WHERE ";
+				}
 				$sql = "$sql t1.$dbfld";
 				if (isset ($$fldtype)){                
-                        switch ($$fldtype) {
+						switch ($$fldtype) {
 							case 1:	$sql = "$sql='".$$fld."'";  break;
 							case 2: $sql = "$sql LIKE '".$$fld."%'";  break;
 							case 3: $sql = "$sql LIKE '%".$$fld."%'";  break;
 							case 4: $sql = "$sql LIKE '%".$$fld."'";
 						}
-                }else{ $sql = "$sql LIKE '%".$$fld."%'"; }
+				}else{ $sql = "$sql LIKE '%".$$fld."%'"; }
 		}
-        return $sql;
-  }  
-  $SQLcmd = '';
-  $SQLcmd = do_field($SQLcmd, 'src', 'src');
-  $SQLcmd = do_field($SQLcmd, 'dst', 'calledstation');
-	
-  
+		return $sql;
+	}  
+	$SQLcmd = '';
+	$SQLcmd = do_field($SQLcmd, 'src', 'src');
+	$SQLcmd = do_field($SQLcmd, 'dst', 'calledstation');
 }
 
 
 $date_clause='';
-// Period (Month-Day)
 if (DB_TYPE == "postgres"){		
-	 	$UNIX_TIMESTAMP = "";
+	$UNIX_TIMESTAMP = "";
 }else{
-		$UNIX_TIMESTAMP = "UNIX_TIMESTAMP";
+	$UNIX_TIMESTAMP = "UNIX_TIMESTAMP";
 }
 $lastdayofmonth = date("t", strtotime($tostatsmonth.'-01'));
 if ($Period=="Month"){
@@ -217,18 +184,6 @@ if ($Period=="Month"){
 	if ($fromday && isset($fromstatsday_sday) && isset($fromstatsmonth_sday)) $date_clause.=" AND $UNIX_TIMESTAMP(t1.starttime) >= $UNIX_TIMESTAMP('$fromstatsmonth_sday-$fromstatsday_sday')";
 	if ($today && isset($tostatsday_sday) && isset($tostatsmonth_sday)) $date_clause.=" AND $UNIX_TIMESTAMP(t1.starttime) <= $UNIX_TIMESTAMP('$tostatsmonth_sday-".sprintf("%02d",intval($tostatsday_sday)/*+1*/)." 23:59:59')";
 }
-//echo "<br>$date_clause<br>";
-/*
-Month
-fromday today
-frommonth tomonth (true)
-fromstatsmonth tostatsmonth
-
-fromstatsday_sday
-fromstatsmonth_sday
-tostatsday_sday
-tostatsmonth_sday
-*/
 
 
   
@@ -240,9 +195,8 @@ if (strpos($SQLcmd, 'WHERE') > 0) {
 
 
 if (!isset ($FG_TABLE_CLAUSE) || strlen($FG_TABLE_CLAUSE)==0){
-		
-		$cc_yearmonth = sprintf("%04d-%02d-%02d",date("Y"),date("n"),date("d"));
-		$FG_TABLE_CLAUSE=" $UNIX_TIMESTAMP(t1.starttime) >= $UNIX_TIMESTAMP('$cc_yearmonth')";
+	$cc_yearmonth = sprintf("%04d-%02d-%02d",date("Y"),date("n"),date("d"));
+	$FG_TABLE_CLAUSE=" $UNIX_TIMESTAMP(t1.starttime) >= $UNIX_TIMESTAMP('$cc_yearmonth')";
 }
 
 
@@ -268,12 +222,11 @@ if ($_SESSION["is_admin"] == 1)
 }
 
 
-
 if ($_SESSION["is_admin"]==0){ 	
 	if (strlen($FG_TABLE_CLAUSE)>0) $FG_TABLE_CLAUSE.=" AND ";
 	$FG_TABLE_CLAUSE.="t1.cardID=t2.IDCust AND t2.IDmanager='".$_SESSION["pr_reseller_ID"]."'";
-	
 }
+
 $FG_ASR_CIC_CLAUSE = $FG_TABLE_CLAUSE;
 //To select just terminatecause=ANSWER
 if (!isset($terminatecause)){
@@ -288,13 +241,22 @@ if ($terminatecause=="ANSWER") {
 if (!$nodisplay){
 	$list = $instance_table -> Get_list ($DBHandle, $FG_TABLE_CLAUSE, $order, $sens, null, null, $FG_LIMITE_DISPLAY, $current_page*$FG_LIMITE_DISPLAY);
 }
-//echo "<br>--<br>".$FG_TABLE_CLAUSE."<br><br>";
-$_SESSION["pr_sql_export"]="SELECT $FG_COL_QUERY FROM $FG_TABLE_NAME WHERE $FG_TABLE_CLAUSE";
+
+// EXPORT 
+// NOTE : MAYBE REWRITE THIS PAGE FROM THE FRAMEWORK
+$FG_EXPORT_SESSION_VAR = "pr_export_entity_call";
+
+// Query Preparation for the Export Functionality
+$_SESSION[$FG_EXPORT_SESSION_VAR]= "SELECT $FG_COL_QUERY FROM $FG_TABLE_NAME WHERE $FG_TABLE_CLAUSE";
+
+if (!is_null ($order) && ($order!='') && !is_null ($sens) && ($sens!='')){
+	$_SESSION[$FG_EXPORT_SESSION_VAR].= " ORDER BY $order $sense";
+}
+
 
 //************************************************************/
 // calculate nbr of success calls,nbr fail calls, max nbr of fail calls successfally /
 //************************************************************/
-
 
 $QUERY="CREATE TEMPORARY TABLE ASR_CIC_TEMP AS (SELECT substring(t1.starttime,1,10) AS day,case when t1.terminatecause='ANSWER' then 1 else 0 end as success,case when t1.terminatecause ='ANSWER'  OR t1.terminatecause='ANSWERED' then 0 else 1 end as fail,0 as maxfail FROM $FG_TABLE_NAME WHERE ".$FG_TABLE_CLAUSE." ORDER BY day)";
 $max_fail=0;
@@ -303,51 +265,51 @@ $total_fail_succ=0;
 $total_max_succ=0;
 $update=array();
 if (!$nodisplay){
-		$num = 0;
-		$res = $DBHandle -> Execute($QUERY);
-		$QUERY="SELECT * FROM ASR_CIC_TEMP order by day";
-		$res = $DBHandle -> Execute($QUERY);
-		if ($res)
-			$num = $res -> RecordCount();
-		$pos=0;
-		for($i=0;$i<$num;$i++)
+	$num = 0;
+	$res = $DBHandle -> Execute($QUERY);
+	$QUERY="SELECT * FROM ASR_CIC_TEMP order by day";
+	$res = $DBHandle -> Execute($QUERY);
+	if ($res)
+		$num = $res -> RecordCount();
+	$pos=0;
+	for($i=0;$i<$num;$i++)
+	{	
+		$asr_cic_list [] =$res -> fetchRow();
+		if ($i>0)
 		{	
-			$asr_cic_list [] =$res -> fetchRow();
-			if ($i>0)
-			{	
-				if ($asr_cic_list[$i][0] == $asr_cic_list[$i-1][0] && $i<$num-1 && $asr_cic_list[$i][2]==1) {
-					$max++;
-				}else {
-					if (($i==$num-1) && ($asr_cic_list[$i][2]==1)) $max++;
-					if ($max > $max_fail) {
-						$max_fail=$max;
-						$asr_cic_list1[$pos][3]=$max_fail;
-						$max=0;
-					}
-					if($asr_cic_list[$i][0] != $asr_cic_list[$i-1][0]){
-						$pos++;
-						$success=0;
- 						$fail=0;
-						$max_fail=0;
-					}
-				}
-				
-			}elseif($asr_cic_list[$i][2]==1){
+			if ($asr_cic_list[$i][0] == $asr_cic_list[$i-1][0] && $i<$num-1 && $asr_cic_list[$i][2]==1) {
 				$max++;
+			}else {
+				if (($i==$num-1) && ($asr_cic_list[$i][2]==1)) $max++;
+				if ($max > $max_fail) {
+					$max_fail=$max;
+					$asr_cic_list1[$pos][3]=$max_fail;
+					$max=0;
+				}
+				if($asr_cic_list[$i][0] != $asr_cic_list[$i-1][0]){
+					$pos++;
+					$success=0;
+					$fail=0;
+					$max_fail=0;
+				}
 			}
-			$success+=$asr_cic_list[$i][1];
- 			$fail+=$asr_cic_list[$i][2];
-			$asr_cic_list1[$pos][0] = $asr_cic_list[$i][0];
-			$asr_cic_list1[$pos][1] = $success; 
-			$asr_cic_list1[$pos][2] = $fail;
-
-			if ($asr_cic_list[$i][2]==1){
-				$total_fail_succ++;	
-			}elseif($total_fail_succ > $total_max_succ){
-				$total_max_succ=$total_fail_succ;
-				$total_fail_succ=0;
-			}
+			
+		}elseif($asr_cic_list[$i][2]==1){
+			$max++;
 		}
+		$success+=$asr_cic_list[$i][1];
+		$fail+=$asr_cic_list[$i][2];
+		$asr_cic_list1[$pos][0] = $asr_cic_list[$i][0];
+		$asr_cic_list1[$pos][1] = $success; 
+		$asr_cic_list1[$pos][2] = $fail;
+		
+		if ($asr_cic_list[$i][2]==1){
+			$total_fail_succ++;	
+		}elseif($total_fail_succ > $total_max_succ){
+			$total_max_succ=$total_fail_succ;
+			$total_fail_succ=0;
+		}
+	}
 }
 
 /************************/
@@ -355,21 +317,19 @@ if (!$nodisplay){
 $QUERY = "SELECT substring(t1.starttime,1,10) AS day, sum(t1.sessiontime) AS calltime, sum(t1.sessionbill) AS cost, count(*) as nbcall, sum(t1.buycost) AS buy FROM $FG_TABLE_NAME WHERE ".$FG_TABLE_CLAUSE." GROUP BY substring(t1.starttime,1,10) ORDER BY day"; //extract(DAY from calldate) 
 
 if (!$nodisplay){
-		$res = $DBHandle -> Execute($QUERY);
-		if ($res){
-			$num = $res -> RecordCount();
-			for($i=0;$i<$num;$i++)
-			{				
-				$list_total_day [] =$res -> fetchRow();
-			}
+	$res = $DBHandle -> Execute($QUERY);
+	if ($res){
+		$num = $res -> RecordCount();
+		for($i=0;$i<$num;$i++)
+		{				
+			$list_total_day [] =$res -> fetchRow();
 		}
-
-
-
-if ($FG_DEBUG == 3) echo "<br>Clause : $FG_TABLE_CLAUSE";
-$nb_record = $instance_table -> Table_count ($DBHandle, $FG_TABLE_CLAUSE);
-if ($FG_DEBUG >= 1) var_dump ($list);
-
+	}
+	
+	if ($FG_DEBUG == 3) echo "<br>Clause : $FG_TABLE_CLAUSE";
+	$nb_record = $instance_table -> Table_count ($DBHandle, $FG_TABLE_CLAUSE);
+	if ($FG_DEBUG >= 1) var_dump ($list);
+	
 }//end IF nodisplay
 
 
@@ -389,18 +349,6 @@ if ($FG_DEBUG == 3) echo "<br>Nb_record : $nb_record";
 if ($FG_DEBUG == 3) echo "<br>Nb_record_max : $nb_record_max";
 
 
-/*******************   TOTAL COSTS  *****************************************
-
-$instance_table_cost = new Table($FG_TABLE_NAME, "sum(t1.costs), sum(t1.buycosts)");		
-if (!$nodisplay){	
-	$total_cost = $instance_table_cost -> Get_list ($DBHandle, $FG_TABLE_CLAUSE, null, null, null, null, null, null);
-}
-*/
-
-
-/*************************************************************/
-
-
 $instance_table_customer = new Table("cc_card", "id,  username, lastname");
 
 $FG_TABLE_CLAUSE = "";
@@ -414,10 +362,8 @@ $list_customer = $instance_table_customer -> Get_list ($DBHandle, $FG_TABLE_CLAU
 $nb_customer = count($list_customer);
 
 
+$smarty->display('main.tpl');
 
-?>
-<?php
-	$smarty->display('main.tpl');
 ?>
 <script language="JavaScript" type="text/JavaScript">
 <!--
@@ -1023,13 +969,21 @@ foreach ($asr_cic_list1 as $asr_cic_data){
 		<td align="center" nowrap="nowrap"><font class="fontstyle_003"><?php if ($totalcost!=0){ display_2dec_percentage((($totalcost - $totalbuycost)/$totalcost)*100); }else{ echo "NULL";} ?></font></td>
 		<td align="center" nowrap="nowrap"><font class="fontstyle_003"><?php if ($totalbuycost!=0){ display_2dec_percentage((($totalcost - $totalbuycost)/$totalbuycost)*100);  }else{ echo "NULL";} ?></font></td>
 	</tr>
-	<!-- FIN TOTAL -->
+	<!-- END TOTAL -->
 
 	  </tbody></table>
-	  <!-- Fin Tableau Global //-->
+	  <!-- END ARRAY GLOBAL //-->
 
 </td></tr></tbody></table>
 
+<br>
+	<!-- SECTION EXPORT //-->
+		 &nbsp; &nbsp; <a href="export_csv.php?var_export=<?php echo $FG_EXPORT_SESSION_VAR ?>&var_export_type=type_csv" target="_blank" ><img src="<?php echo Images_Path;?>/excel.gif" border="0" height="30"/><?php echo gettext("Export CSV");?></a>
+		
+		 - &nbsp; &nbsp; <a href="export_csv.php?var_export=<?php echo $FG_EXPORT_SESSION_VAR ?>&var_export_type=type_xml" target="_blank" ><img src="<?php echo Images_Path;?>/icons_xml.gif" border="0" height="32"/><?php echo gettext("Export XML");?></a>
+		
+		
+		
 <?php  }else{ ?>
 	<center><h3><?php echo gettext("No calls in your selection");?>.</h3></center>
 <?php  } ?>
