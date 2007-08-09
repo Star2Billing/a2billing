ebca3d1877bf86f3cf4081ce207c7b797a0d4405
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-08-09 08:16:50 +0000

    BUG : Steve Dommet ticket #33 - bad date check on postgresql
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@142 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- A2Billing_AGI/a2billing.php
+++ A2Billing_AGI/a2billing.php
@@ -345,8 +345,12 @@ if ($mode == 'standard'){
 		$QUERY =  "SELECT cc_did.id, cc_did_destination.id, billingtype, tariff, destination,  voip_call, username".
 			" FROM cc_did, cc_did_destination,  cc_card ".
 			" WHERE id_cc_did=cc_did.id and cc_card.id=id_cc_card and cc_did_destination.activated=1  and cc_did.activated=1 and did='$mydnid' ".
-			" AND cc_did.startingdate<= CURRENT_TIMESTAMP AND (cc_did.expirationdate > CURRENT_TIMESTAMP OR cc_did.expirationdate IS NULL OR ".
-			" cc_did.expirationdate = '0000-00-00 00:00:00') ORDER BY priority ASC";
+			" AND cc_did.startingdate<= CURRENT_TIMESTAMP AND (cc_did.expirationdate > CURRENT_TIMESTAMP OR cc_did.expirationdate IS NULL";
+		if ($A2B->config["database"]['dbtype'] != "postgres"){
+			// MYSQL
+			$QUERY .= " OR cc_did.expirationdate = '0000-00-00 00:00:00'";
+		}
+		$QUERY .= ") ORDER BY priority ASC";
 		
 		$A2B -> debug( VERBOSE | WRITELOG, $agi, __FILE__, __LINE__, $QUERY);
 		$result = $A2B -> instance_table -> SQLExec ($A2B->DBHandle, $QUERY);
