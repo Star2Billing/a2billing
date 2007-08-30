b3ea4a3f27c39f0ebdbd88981a74c6f010c0d7b2
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-08-30 18:52:31 +0000

    UPDATE : invoice list
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@193 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- A2Billing_UI/Public/form_data/FG_var_invoice_list.inc.php
+++ A2Billing_UI/Public/form_data/FG_var_invoice_list.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-$plang='en';
 getpost_ifset(array('cardid','searchenabled','monthselect','yearselect','paymentoperator','totaloperator','paymenttext','totaltext','filterradio','section'));
 if ($totaltext == "")
 {
@@ -50,25 +49,15 @@ else
 	$HD_Form -> FG_TABLE_CLAUSE = "inv.cardid = $cardid";
 }
 
-//echo $HD_Form -> FG_TABLE_CLAUSE;
-
 //$HD_Form -> FG_TABLE_CLAUSE = "inv.cardid = $cardid";
 $HD_Form -> FG_TABLE_DEFAULT_ORDER = "inv.invoicecreated_date";
 $HD_Form -> FG_TABLE_DEFAULT_SENS = "DESC";
 $HD_Form -> FG_DEBUG = 0;
 $HD_Form -> FG_LIMITE_DISPLAY = 10;
-// FAQ
 
-// Weird Hack to allow TO PAY
-$QUERY="SELECT id from cc_card";
+
 $QUERY_REFILL="SELECT t1.id, sum(t3.credit) from cc_card as t1, cc_logrefill as t3 WHERE t1.id=t3.card_id GROUP BY t1.id";
 $QUERY_PAYMENT="SELECT t1.id, SUM(t2.payment) from cc_card as t1, cc_logpayment as t2 WHERE t1.id=t2.card_id GROUP BY t1.id";
-
-$instance = new Table();
-$DBhandle = DbConnect();
-$to_pay = $instance -> SQLExec($DBhandle,$QUERY);
-
-//QUERY_CREDIT=SELECT t1.id, SUM(t1.credit) from cc_card as t1 GROUP BY t1.id
 
 $HD_Form -> AddViewElement(gettext("Invoice Date"), "invoicecreated_date", "12%", "center", "sort", "22", "", "", "", "", "", "display_dateonly");
 $HD_Form -> AddViewElement(gettext("Cover Start"), "cover_startdate", "12%", "center", "sort", "22", "", "", "", "", "", "display_dateonly");
@@ -90,19 +79,18 @@ $HD_Form -> CV_DISPLAY_FILTER_ABOVE_TABLE = false;
 $HD_Form -> FG_EDITION = false;
 $HD_Form -> FG_DELETION = false;
 
-	$HD_Form -> FG_OTHER_BUTTON1 = true;
-	$HD_Form -> FG_OTHER_BUTTON2 = true;
-	
-	$HD_Form -> FG_OTHER_BUTTON1_LINK="A2B_entity_invoice_detail.php?id=|param|&invoice_type=2";
-	$HD_Form -> FG_OTHER_BUTTON2_LINK="A2B_entity_invoice_detail_pdf.php?id=|param|&cardid=$cardid&action=sendinvoice&exporttype=pdf&invoice_type=2";
-	
-	$HD_Form -> FG_OTHER_BUTTON1_ALT = 'Details';
-	$HD_Form -> FG_OTHER_BUTTON2_ALT = 'Send';
-		
-	$HD_Form -> FG_OTHER_BUTTON1_IMG = Images_Path.'/details.gif';
-	$HD_Form -> FG_OTHER_BUTTON2_IMG = Images_Path.'/email03.gif';
+$HD_Form -> FG_OTHER_BUTTON1 = true;
+$HD_Form -> FG_OTHER_BUTTON2 = true;
 
-	//$HD_Form -> FG_OTHER_BUTTON2_IMG = '';
+$HD_Form -> FG_OTHER_BUTTON1_LINK="A2B_entity_invoice_detail.php?id=|param|&invoice_type=2";
+$HD_Form -> FG_OTHER_BUTTON2_LINK="A2B_entity_invoice_detail_pdf.php?id=|param|&cardid=$cardid&action=sendinvoice&exporttype=pdf&invoice_type=2";
+
+$HD_Form -> FG_OTHER_BUTTON1_ALT = 'Details';
+$HD_Form -> FG_OTHER_BUTTON2_ALT = 'Send';
+	
+$HD_Form -> FG_OTHER_BUTTON1_IMG = Images_Path.'/details.gif';
+$HD_Form -> FG_OTHER_BUTTON2_IMG = Images_Path.'/email03.gif';
+
 
 $HD_Form -> FG_INTRO_TEXT_EDITION= gettext("You can modify, through the following form, the different properties of your ".$HD_Form->FG_INSTANCE_NAME);
 $HD_Form -> FG_INTRO_TEXT_ASK_DELETION = gettext("If you really want remove this ".$HD_Form->FG_INSTANCE_NAME.", click on the delete button.");
@@ -128,4 +116,3 @@ $HD_Form -> FG_BUTTON_EDITION_BOTTOM_TEXT = $HD_Form -> FG_BUTTON_ADITION_BOTTOM
 $HD_Form -> FG_GO_LINK_AFTER_ACTION_ADD = $_SERVER['PHP_SELF']."?atmenu=document&stitle=Document&wh=AC&id=";
 $HD_Form -> FG_GO_LINK_AFTER_ACTION_EDIT = $_SERVER['PHP_SELF']."?atmenu=document&stitle=Document&wh=AC&id=";
 $HD_Form -> FG_GO_LINK_AFTER_ACTION_DELETE = $_SERVER['PHP_SELF']."?atmenu=document&stitle=Document&wh=AC&id=";
-?>
