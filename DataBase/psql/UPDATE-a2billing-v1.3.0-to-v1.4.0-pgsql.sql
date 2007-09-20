8102cb94e7aef76435c98b4f2fa59d0a539dc001
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-09-20 11:30:48 +0000

    UPDATE : Add indexes and OPtimize tables
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@295 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/psql/UPDATE-a2billing-v1.3.0-to-v1.4.0-pgsql.sql
+++ DataBase/psql/UPDATE-a2billing-v1.3.0-to-v1.4.0-pgsql.sql
@@ -601,3 +601,21 @@ CREATE TABLE cc_card_history (
 	description 					TEXT
 );
 ALTER TABLE ONLY cc_card_history    ADD CONSTRAINT cc_card_history_pkey PRIMARY KEY (id);
+
+
+
+-- Never too late to add some indexes :D
+
+CREATE INDEX cc_call_username_ind ON cc_call USING btree (username); 	
+CREATE INDEX cc_call_starttime_ind ON cc_call USING btree (starttime);	
+CREATE INDEX cc_call_terminatecause_ind ON cc_call USING btree (terminatecause); 	
+CREATE INDEX cc_call_calledstation_ind ON cc_call USING btree (calledstation); 	
+
+
+CREATE INDEX cc_card_creationdate_ind ON cc_card USING btree (creationdate);
+CREATE INDEX cc_card_username_ind ON cc_card USING btree (username);
+
+
+VACUUM FULL ANALYZE;
+-- Change bellow your dbname & uncomment
+-- REINDEX DATABASE a2billing;
