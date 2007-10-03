7fbb5db171e20172d81cff5942b3e3c685ba710e
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-10-03 09:40:25 +0000

    UPDATE : clean CC_entity_sim_ratecard.php
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@354 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- A2Billing_UI/Public/CC_entity_sim_ratecard.php
+++ A2Billing_UI/Public/CC_entity_sim_ratecard.php
@@ -4,10 +4,11 @@ include ("../lib/module.access.php");
 include ("../lib/Class.RateEngine.php");	
 include ("../lib/smarty.php");
 
-if (! has_rights (ACX_RATECARD)){ 
-	   Header ("HTTP/1.0 401 Unauthorized");
-	   Header ("Location: PP_error.php?c=accessdenied");	   
-	   die();	   
+
+if (! has_rights (ACX_RATECARD)) {
+	Header ("HTTP/1.0 401 Unauthorized");
+	Header ("Location: PP_error.php?c=accessdenied");	   
+	die();
 }
 
 getpost_ifset(array('posted', 'tariffplan', 'balance', 'id_cc_card', 'called'));
@@ -16,79 +17,62 @@ getpost_ifset(array('posted', 'tariffplan', 'balance', 'id_cc_card', 'called'));
 $FG_DEBUG = 0;
 $DBHandle  = DbConnect();
 
-/*
-if (is_string ($tariffplan) && strlen(trim($tariffplan))>0){
+if ($called  && $id_cc_card) {
+	
+	$calling=ereg_replace("^\+","011",$called);	
+	$calling=ereg_replace("[^0-9]","",$calling);	
+	$calling=ereg_replace("^01100","011",$calling);	
+	$calling=ereg_replace("^00","011",$calling);	
+	$calling=ereg_replace("^0111","1",$calling);
+	
+	if ( strlen($calling)>2 && is_numeric($calling)) {
 		
-		list($sim_mytariff_id, $sim_mytariffname) = split('-:-', $tariffplan);		
-		$_SESSION["sim_mytariff_id"]= $sim_mytariff_id;
-		$_SESSION["sim_mytariffname"]= $sim_mytariffname;
-		//$_SESSION["basetariffgroup"]= $basetariffgroup;		
-}else{
-		$sim_mytariff_id = $_SESSION["sim_mytariff_id"];
-		$sim_mytariffname = $_SESSION["sim_mytariffname"];
-		//$basetariffgroup = $_SESSION["basetariffgroup"];	
-}
-if ($FG_DEBUG == 1)  echo "sim_mytariff_id:$sim_mytariff_id<br>";
-if ($FG_DEBUG == 1)  echo "sim_mytariffname:$sim_mytariffname<br>";
-*/
-
-
-if ($called  && $id_cc_card){
+		$A2B -> DBHandle = DbConnect();
+		$instance_table = new Table();
+		$A2B -> set_instance_table ($instance_table);
+		$num = 0;
+		$QUERY = "SELECT username, tariff, credit FROM cc_card where id='$id_cc_card'";
+		$resmax = $DBHandle -> Execute($QUERY);
+		if ($resmax)
+			$num = $resmax -> RecordCount( );
 		
-		
-		$calling=ereg_replace("^\+","011",$called);	
-		$calling=ereg_replace("[^0-9]","",$calling);	
-		$calling=ereg_replace("^01100","011",$calling);	
-		$calling=ereg_replace("^00","011",$calling);	
-		$calling=ereg_replace("^0111","1",$calling);
-		
-		if ( strlen($calling)>2 && is_numeric($calling)){
-			
-			$A2B -> DBHandle = DbConnect();
-			$instance_table = new Table();
-			$A2B -> set_instance_table ($instance_table);
-			$num = 0;
-			$QUERY = "SELECT username, tariff, credit FROM cc_card where id='$id_cc_card'";
-			$resmax = $DBHandle -> Execute($QUERY);
-			if ($resmax)
-				$num = $resmax -> RecordCount( );
-			
-			if ($num==0){ echo gettext("Error card !!!"); exit();}			
-			
-			for($i=0;$i<$num;$i++)
-			{
-				$row [] =$resmax -> fetchRow();	
-			}
-			
-			$A2B -> cardnumber = $row[0][0];
-			$A2B -> credit = $balance = $row[0][2];
-			if ($FG_DEBUG == 1) echo "cardnumber = ".$row[0][0] ."<br>";
-			
-			if ($A2B -> callingcard_ivr_authenticate_light ($error_msg)){
-				if ($FG_DEBUG == 1) $RateEngine -> debug_st = 1;
-				
-				$RateEngine = new RateEngine();
-				$RateEngine -> webui = 0;
-				// LOOKUP RATE : FIND A RATE FOR THIS DESTINATION
-				
-				$A2B ->agiconfig['accountcode'] = $A2B -> cardnumber ;
-				$A2B ->agiconfig['use_dnid']=1;
-				$A2B ->agiconfig['say_timetocall']=0;						
-				$A2B ->dnid = $A2B ->destination = $calling;
-				
-				if ($A2B->removeinterprefix) $A2B->destination = $A2B -> apply_rules ($A2B->destination);			
-				
-				$resfindrate = $RateEngine->rate_engine_findrates($A2B, $A2B->destination, $row[0][1]);
-				if ($FG_DEBUG == 1) echo "resfindrate=$resfindrate";
-				
-				// IF FIND RATE
-				if ($resfindrate!=0){	
-					$res_all_calcultimeout = $RateEngine->rate_engine_all_calcultimeout($A2B, $A2B->credit);
-					if ($FG_DEBUG == 1) print_r($RateEngine->ratecard_obj);
-				}
-			}
-			
+		if ($num==0) {
+			echo gettext("Error card !!!"); exit();
 		}
+		
+		for($i=0;$i<$num;$i++) {
+			$row [] =$resmax -> fetchRow();	
+		}
+		
+		$A2B -> cardnumber = $row[0][0];
+		$A2B -> credit = $balance = $row[0][2];
+		if ($FG_DEBUG == 1) echo "cardnumber = ".$row[0][0] ."<br>";
+		
+		if ($A2B -> callingcard_ivr_authenticate_light ($error_msg)){
+			if ($FG_DEBUG == 1) $RateEngine -> debug_st = 1;
+			
+			$RateEngine = new RateEngine();
+			$RateEngine -> webui = 0;
+			// LOOKUP RATE : FIND A RATE FOR THIS DESTINATION
+			
+			$A2B ->agiconfig['accountcode'] = $A2B -> cardnumber ;
+			$A2B ->agiconfig['use_dnid']=1;
+			$A2B ->agiconfig['say_timetocall']=0;						
+			$A2B ->dnid = $A2B ->destination = $calling;
+			
+			if ($A2B->removeinterprefix) $A2B->destination = $A2B -> apply_rules ($A2B->destination);			
+			
+			$resfindrate = $RateEngine->rate_engine_findrates($A2B, $A2B->destination, $row[0][1]);
+			if ($FG_DEBUG == 1) echo "resfindrate=$resfindrate";
+			
+			// IF FIND RATE
+			if ($resfindrate!=0){	
+				$res_all_calcultimeout = $RateEngine->rate_engine_all_calcultimeout($A2B, $A2B->credit);
+				if ($FG_DEBUG == 1) print_r($RateEngine->ratecard_obj);
+			}
+		}
+		
+	}
 }
 
 /**************************************************************/
@@ -105,121 +89,56 @@ $nb_tariffname = count($list_tariffname);
 /*************************************************************/
 
 
+
+$smarty->display('main.tpl');
+
+
+echo $CC_help_sim_ratecard;
+
 ?>
-
-<?php
-	$smarty->display('main.tpl');
-?>
-
-<script language="JavaScript" type="text/JavaScript">
-<!--
-function MM_openBrWindow(theURL,winName,features) { //v2.0
-  window.open(theURL,winName,features);
-}
-
-function openURL(theLINK)
-{
 	
-	// grab index number of the selected option
-	selInd = document.theForm.choose_list.selectedIndex;
-	if(selInd==''){alert('Please, select a tariff'); return false;}
-	// get value of the selected option
-	goURL = document.theForm.choose_list.options[selInd].value;
-	  
-	definecredit = document.theForm.definecredit.value;
-	// redirect browser to the grabbed value (hopefully a URL)	  
-	self.location.href = theLINK + goURL + "&definecredit="+definecredit ; //+ "&opt="+opt;
-}
-
-
-
-//-->
-</script>
-
-
-<?php
-	echo $CC_help_sim_ratecard;
-?>
-
-<?php  if (false){ ?>
-	  <center>
-	  <?php  if (is_string ($sim_mytariffname)) echo "<font size=\"3\">".gettext("THE CURRENT RATECARD")." : <b>$sim_mytariffname</b></font><br><br>"; ?>
-	  
-	  <!-- ** ** ** ** ** Part for the research ** ** ** ** ** -->
 	
-	<FORM METHOD=POST ACTION="<?php echo $PHP_SELF?>?s=1&t=0&order=<?php echo $order?>&sens=<?php echo $sens?>&current_page=<?php echo $current_page?>">
-	<INPUT TYPE="hidden" NAME="posted" value=1>
-	<INPUT TYPE="hidden" NAME="current_page" value=0>	
-		<table class="bar-status" width="75%" border="0" cellspacing="1" cellpadding="2" align="center">
-			<tbody>
-			<tr>
-				<td align="left" valign="top" class="bgcolor_004">					
-					<font class="fontstyle_003">&nbsp;&nbsp;<?php echo gettext("R A T E C A R D");?></font>
-				</td>				
-				<td class="bgcolor_005" align="left" >
-				<table width="100%" border="0" cellspacing="0" cellpadding="0" class="bgcolor_005"><tr>
-					<td width="50%" align="center">&nbsp;&nbsp;
-						<select NAME="tariffplan" size="1"  style="width=250" class="form_input_select">
-								<option value=''><?php echo gettext("Choose a ratecard");?></option>
-							
-								<?php					 
-								 foreach ($list_tariffname as $recordset){ 						 
-								?>
-									<option class=input value='<?php  echo $recordset[0]."-:-".$recordset[1]?>' <?php if ($recordset[0]==$tariffplan) echo "selected";?>><?php echo $recordset[1]?></option>                        
-								<?php 	 }
-								?>
-						</select>						
-					</td>
-					<td class="bgcolor_003" align="center"  width="50%">
-					<input type="image"  name="image16" align="top" border="0" src="<?php echo Images_Path;?>/button-search.gif" />
-	  			</td>
-				
-				</tr></table></td>
-			</tr>							
-		</tbody></table>
-	</FORM>
-</center>
-<?php  } ?>
-
 	<center> <?php echo "$error_msg"; ?> </center>
-	  <br>
-	  <table width="<?php echo $FG_HTML_TABLE_WIDTH?>" border="0" align="center" cellpadding="0" cellspacing="0">
-		<TR>
-          <TD style="border-bottom: medium dotted #8888CC" colspan="2"> <B><?php echo gettext("RATECARD SIMULATOR");?></B></TD>
-        </TR>
-		<FORM NAME="theFormFilter" action="<?php echo $PHP_SELF?>">		
-		<tr>			
-            <td height="31" style="padding-left: 5px; padding-right: 3px;" class="bgcolor_009">
-					<br><font color="white"><b><?php echo gettext("NUMBER YOU WISH TO CALL");?> :</b></font>
-					<INPUT type="text" name="called" value="<?php echo $called;?>" class="form_input_text">
-					<br>
-					<?php if (false){ ?>
-					<br>
-					<font color="white"><b><?php echo gettext("YOUR BALANCE");?> :</b></font>
-					<INPUT type="text" class="form_input_text" name="balance" value="<?php if (!isset($balance)) echo "10"; else echo $balance;?>"> 
-					<?php } ?>
+	<br>
+	<table width="<?php echo $FG_HTML_TABLE_WIDTH?>" border="0" align="center" cellpadding="0" cellspacing="0">
+	<TR>
+	  <TD style="border-bottom: medium dotted #8888CC" colspan="2"> <B><?php echo gettext("RATECARD SIMULATOR");?></B></TD>
+	</TR>
+	<FORM NAME="theFormFilter" action="<?php echo $PHP_SELF?>">		
+	<tr>			
+		<td height="31" style="padding-left: 5px; padding-right: 3px;" class="bgcolor_009">
+				<br><font color="white"><b><?php echo gettext("NUMBER YOU WISH TO CALL");?> :</b></font>
+				<INPUT type="text" name="called" value="<?php echo $called;?>" class="form_input_text">
+				<br>
+				<?php if (false){ ?>
+				<br>
+				<font color="white"><b><?php echo gettext("YOUR BALANCE");?> :</b></font>
+				<INPUT type="text" class="form_input_text" name="balance" value="<?php if (!isset($balance)) echo "10"; else echo $balance;?>"> 
+				<?php } ?>
+				
+				<br>
+				 <input class="form_input_text" name="id_cc_card" size="30" maxlength="50" value="<?php echo $id_cc_card;?>"> 
+					<a href="#" onclick="window.open('A2B_entity_card.php?popup_select=1&popup_formname=theFormFilter&popup_fieldname=id_cc_card' , 'CardNumberSelection','width=550,height=330,top=20,left=100,scrollbars=1');"><img src="<?php echo Images_Path;?>/icon_arrow_orange.gif"></a>
 					
-					<br>
-					 <input class="form_input_text" name="id_cc_card" size="30" maxlength="50" value="<?php echo $id_cc_card;?>"> 
-						<a href="#" onclick="window.open('A2B_entity_card.php?popup_select=1&popup_formname=theFormFilter&popup_fieldname=id_cc_card' , 'CardNumberSelection','width=550,height=330,top=20,left=100,scrollbars=1');"><img src="<?php echo Images_Path;?>/icon_arrow_orange.gif"></a>
-						
-                       <?php echo gettext("Select the card number ID to use");?>.
-					<br>
-					
-			</td>
-			<td height="31" class="bgcolor_009" style="padding-left: 5px; padding-right: 3px;">
-				<input type="SUBMIT" value="<?php echo gettext("SIMULATE");?>"  class="form_input_button"/>
-			</td>
-        </tr>
-		
-		</FORM>	
-		<TR> 
-          <TD style="border-bottom: medium dotted #8888CC"  colspan="2"><br></TD>
-        </TR>
-	  </table>
-	  
-	  
-<?php if ( (is_array($RateEngine->ratecard_obj)) && (!empty($RateEngine->ratecard_obj)) ){
+				   <?php echo gettext("Select the card number ID to use");?>.
+				<br>
+				
+		</td>
+		<td height="31" class="bgcolor_009" style="padding-left: 5px; padding-right: 3px;">
+			<input type="SUBMIT" value="<?php echo gettext("SIMULATE");?>"  class="form_input_button"/>
+		</td>
+	</tr>
+	
+	</FORM>	
+	<TR> 
+	  <TD style="border-bottom: medium dotted #8888CC"  colspan="2"><br></TD>
+	</TR>
+	</table>
+	
+	
+<?php
+
+if ( (is_array($RateEngine->ratecard_obj)) && (!empty($RateEngine->ratecard_obj)) ){
 if ($FG_DEBUG == 1) print_r($RateEngine->ratecard_obj);
 
 $arr_ratecard=array('tariffgroupname', 'lcrtype', 'idtariffgroup', 'cc_tariffgroup_plan.idtariffplan', 'tariffname', 'destination', 'cc_ratecard.id' , 'dialprefix', 'destination', 'buyrate', 'buyrateinitblock', 'buyrateincrement', 'rateinitial', 'initblock', 'billingblock', 'connectcharge', 'disconnectcharge', 'stepchargea', 'chargea', 'timechargea', 'billingblocka', 'stepchargeb', 'chargeb', 'timechargeb', 'billingblockb', 'stepchargec', 'chargec', 'timechargec', 'billingblockc', 'tp_id_trunk', 'tp_trunk', 'providertech', 'tp_providerip', 'tp_removeprefix');
@@ -280,10 +199,12 @@ $FG_TABLE_ALTERNATE_ROW_COLOR[1]='#EEE9E9';
           <TD style="border-bottom: medium dotted #8888CC"  colspan="2"><br></TD>
         </TR>
 	  </table>
-<?php  }else{ ?>
-<br><br><br><br>
-<?php  } ?>
+<?php  } else { ?>
 
-<?php
+<br><br><br><br>
+
+<?php  } 
+
 	$smarty->display('footer.tpl');
+
 ?>
