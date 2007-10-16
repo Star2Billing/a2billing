74ef0be85e2975557609285b3209e91905224f8c
stavros <stavros@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-10-16 20:07:16 +0000

    BUG: The mail template for epaymentverify was omitted from the 1.2.3->1.3.0 upgrade, and was impossible to add via the GUI
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@390 9d51a8f0-6521-42ea-932e-d822aa02ce2f

---
+++ DataBase/psql/UPDATE-a2billing-v1.3.0-to-v1.3.1-pgsql.sql
@@ -0,0 +1,13 @@
+INSERT INTO cc_templatemail VALUES ('epaymentverify', 'info@call-labs.com', 'Call-Labs', 'Epayment Gateway Security Verification Failed', 'Dear Administrator
+
+Please check the Epayment Log, System has logged a Epayment Security failure. that may be a possible attack on epayment processing.
+
+Time of Transaction: $time
+Payment Gateway: $paymentgateway
+Amount: $amount
+
+
+
+Kind regards,
+Call Labs
+', '');
