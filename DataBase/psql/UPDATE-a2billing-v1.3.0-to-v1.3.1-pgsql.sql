2132bb2aa65344ea51124440add5acd3ed9ceefa
stavros <stavros@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-10-20 19:19:40 +0000

    UPDATE: fix stupid mistakes in previous ISO-3166-1 update
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@397 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/psql/UPDATE-a2billing-v1.3.0-to-v1.3.1-pgsql.sql
+++ DataBase/psql/UPDATE-a2billing-v1.3.0-to-v1.3.1-pgsql.sql
@@ -13,20 +13,20 @@ Call Labs
 ', '');
 
 -- fix various uses of ISO-3166-1 alpha-2 rather than alpha-3
-UPDATE cc_country SET countryprefix='BVT' WHERE countryprefix='BV';
-UPDATE cc_country SET countryprefix='IOT' WHERE countryprefix='IO';
-UPDATE cc_country SET countryprefix='HMD' WHERE countryprefix='HM';
-UPDATE cc_country SET countryprefix='PCN' WHERE countryprefix='PC';
-UPDATE cc_country SET countryprefix='SGS' WHERE countryprefix='GS';
-UPDATE cc_country SET countryprefix='SJM' WHERE countryprefix='SJ';
-UPDATE cc_country SET countryprefix='TLS' WHERE countryprefix='TL';
-UPDATE cc_country SET countryprefix='UMI' WHERE countryprefix='UM';
-UPDATE cc_country SET countryprefix='ESH' WHERE countryprefix='EH';
+UPDATE cc_country SET countrycode='BVT' WHERE countrycode='BV';
+UPDATE cc_country SET countrycode='IOT' WHERE countrycode='IO';
+UPDATE cc_country SET countrycode='HMD' WHERE countrycode='HM';
+UPDATE cc_country SET countrycode='PCN' WHERE countrycode='PN';
+UPDATE cc_country SET countrycode='SGS' WHERE countrycode='GS';
+UPDATE cc_country SET countrycode='SJM' WHERE countrycode='SJ';
+UPDATE cc_country SET countrycode='TLS' WHERE countrycode='TL';
+UPDATE cc_country SET countrycode='UMI' WHERE countrycode='UM';
+UPDATE cc_country SET countrycode='ESH' WHERE countrycode='EH';
 
 -- integrate changes from ISO-3166-1 newsletters V-1 to V-12
-UPDATE cc_country SET countryname='Lao People''s Democratic Republic' WHERE countryprefix='LAO';
-UPDATE cc_country SET countryname='Timor-Leste', countryprefix='670' WHERE countryprefix='TLS';
-UPDATE cc_country SET countryprefix='0' WHERE countryprefix='TMP';
+UPDATE cc_country SET countryname='Lao People''s Democratic Republic' WHERE countrycode='LAO';
+UPDATE cc_country SET countryname='Timor-Leste', countryprefix='670' WHERE countrycode='TLS';
+UPDATE cc_country SET countryprefix='0' WHERE countrycode='TMP';
 INSERT INTO cc_country VALUES (246, 'ALA' ,'35818', 'Aland Islands');
 INSERT INTO cc_country VALUES (247, 'BLM' ,'0', 'Saint Barthelemy');
 INSERT INTO cc_country VALUES (248, 'GGY' ,'441481', 'Guernsey');
