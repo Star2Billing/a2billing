a94fd0ae52f881acfd940bee1f42648399aa2bdb
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2007-12-04 10:45:13 +0000

    UPDATE : misc on Class Form
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@458 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- A2Billing_UI/Public/A2B_entity_friend.php
+++ A2Billing_UI/Public/A2B_entity_friend.php
@@ -5,7 +5,7 @@ include ("../lib/Form/Class.FormHandler.inc.php");
 include ("./form_data/FG_var_friend.inc");
 include ("../lib/smarty.php");
 
-if (! has_rights (ACX_CUSTOMER)){ 
+if (! has_rights (ACX_CUSTOMER)) {
 	Header ("HTTP/1.0 401 Unauthorized");
 	Header ("Location: PP_error.php?c=accessdenied");	   
 	die();
