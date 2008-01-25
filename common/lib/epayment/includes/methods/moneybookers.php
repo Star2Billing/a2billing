9ada5a9717b79710a9db242fb012c86254409fe6
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-01-25 11:48:54 +0000

    UPDATE : common lib epayment
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk_v2@556 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- common/lib/epayment/includes/methods/moneybookers.php
+++ common/lib/epayment/includes/methods/moneybookers.php
@@ -1,20 +1,11 @@
 <?php
-/*
-  $Id: moneybookers.php,v 2.0 2004/04/20 12:00:00 by Templatefactory.info
 
-  osCommerce, Open Source E-Commerce Solutions
-  http://www.oscommerce.com
-
-  Copyright (c) 2002 osCommerce
-
-  Released under the GNU General Public License
-*/
-
-  define('MODULE_PAYMENT_MONEYBOOKERS_TEXT_TITLE', 'Moneybookers.com');
-  define('MODULE_PAYMENT_MONEYBOOKERS_TEXT_DESCRIPTION', 'Moneybookers.com');
-  define('MODULE_PAYMENT_MONEYBOOKERS_REFID', '811621');
-  define('MODULE_PAYMENT_MONEYBOOKERS_CURRENCY', 'USD');
-  define('MODULE_PAYMENT_MONEYBOOKERS_ID', 'info@areski.net');
-  define('STORE_NAME', 'A2Billing');
-  define('MODULE_PAYMENT_MONEYBOOKERS_LANGUAGE', 'EN');
+	define('MODULE_PAYMENT_MONEYBOOKERS_TEXT_TITLE', 'Moneybookers.com');
+	define('MODULE_PAYMENT_MONEYBOOKERS_TEXT_DESCRIPTION', 'Moneybookers.com');
+	//  define('MODULE_PAYMENT_MONEYBOOKERS_REFID', '811621');
+	//  define('MODULE_PAYMENT_MONEYBOOKERS_CURRENCY', 'USD');
+	//  define('MODULE_PAYMENT_MONEYBOOKERS_ID', 'info@areski.net');
+	define('STORE_NAME', 'A2Billing');
+	//  define('MODULE_PAYMENT_MONEYBOOKERS_LANGUAGE', 'EN');
+	
 ?>
