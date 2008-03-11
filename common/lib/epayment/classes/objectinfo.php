b05100684d2286c653c290676340ba28b4ec9783
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-03-11 18:03:29 +0000

    ident
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@613 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- common/lib/epayment/classes/objectinfo.php
+++ common/lib/epayment/classes/objectinfo.php
@@ -1,4 +1,6 @@
 <?php
+
+
 /*
   $Id: object_info.php,v 1.6 2003/06/20 16:23:08 hpdl Exp $
 
@@ -10,14 +12,14 @@
   Released under the GNU General Public License
 */
 
-  class objectInfo {
+class objectInfo {
 
-// class constructor
-    function objectInfo($object_array) {
-      reset($object_array);
-      while (list($key, $value) = each($object_array)) {
-        $this->$key = tep_db_prepare_input($value);
-      }
-    }
-  }
+	// class constructor
+	function objectInfo($object_array) {
+		reset($object_array);
+		while (list ($key, $value) = each($object_array)) {
+			$this-> $key = tep_db_prepare_input($value);
+		}
+	}
+}
 ?>
