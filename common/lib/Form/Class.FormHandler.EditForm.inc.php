f36d07ebf908d61bc2187e088d67af992f68e9db
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-05-07 14:46:19 +0000

    fix with image link
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@706 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- common/lib/Form/Class.FormHandler.EditForm.inc.php
+++ common/lib/Form/Class.FormHandler.EditForm.inc.php
@@ -104,7 +104,7 @@ function sendtolittle(direction){
 				{
                         ?>
                          <INPUT class="form_enter" name=<?php echo $this->FG_TABLE_EDITION[$i][1]?>  <?php echo $this->FG_TABLE_EDITION[$i][4]?> value="<?php if($this->VALID_SQL_REG_EXP){ echo stripslashes($list[0][$i]); }else{ echo $_POST[$this->FG_TABLE_ADITION[$i][1]]; }?>">
-                          <a href="javascript:cal<?php echo $this->FG_TABLE_EDITION[$i][1]?>.popup();"><img src="img/cal.gif" width="16" height="16" border="0" title="Click Here to Pick up the date" alt="Click Here to Pick up the date"></a>
+                          <a href="javascript:cal<?php echo $this->FG_TABLE_EDITION[$i][1]?>.popup();"><img src="<?php echo Images_Path_Main;?>/cal.gif" width="16" height="16" border="0" title="Click Here to Pick up the date" alt="Click Here to Pick up the date"></a>
                           <script language="JavaScript">
                          <!-- // create calendar object(s) just after form tag closed
                              // specify form element as the only parameter (document.forms['formname'].elements['inputname']);
