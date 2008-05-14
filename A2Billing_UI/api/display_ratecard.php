16c4bbef86ccaf4e1bb6ebc222860fbb03853ccf
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-05-14 14:32:54 +0000

    fix on display ratecard
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@721 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- A2Billing_UI/api/display_ratecard.php
+++ A2Billing_UI/api/display_ratecard.php
@@ -258,10 +258,10 @@ if ( is_null ($order) || is_null($sens) ){
 $instance_table = new Table($FG_TABLE_NAME, $FG_COL_QUERY);
 $list = $instance_table -> Get_list ($DBHandle, $FG_TABLE_CLAUSE, $order, $sens, null, null, $FG_LIMITE_DISPLAY, $current_page*$FG_LIMITE_DISPLAY, $sql_group, $caching_query);
 
-$country_table = new Table("cc_country","countryname");
-$country_list = $country_table -> Get_list ($DBHandle);
-
-$QUERY="SELECT count(*) from $FG_TABLE_NAME WHERE $FG_TABLE_CLAUSE";
+$country_table = new Table("cc_country","countryname"); 
+$country_list = $country_table -> Get_list ($DBHandle); 
+ 
+$QUERY="SELECT count(*) FROM (SELECT t1.* FROM $FG_TABLE_NAME WHERE $FG_TABLE_CLAUSE".$sql_group.") as setprefix "; 
 
 $list_nrecord=$instance_table->SQLExec($DBHandle,$QUERY,1,$caching_query);
 $nb_record = $list_nrecord[0][0];
