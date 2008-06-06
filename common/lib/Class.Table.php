81e7d7bd8f6cc1d870844e10c962a90898323ad8
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-06-06 11:21:43 +0000

    changes on Table Class
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@794 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- common/lib/Class.Table.php
+++ common/lib/Class.Table.php
@@ -9,7 +9,7 @@
 * PHP versions 4 and 5
 *
 * A2Billing -- Asterisk billing solution.
-* Copyright (C) 2004-2007 : A2Billing
+* Copyright (C) 2004-2008 : A2Billing
 *
 * See http://www.asterisk2billing.org for more information about
 * the A2Billing project.
@@ -21,7 +21,7 @@
 * @category   Database
 * @package    Table
 * @author     Arezqui Belaid <areski _atl_ gmail com>
-* @copyright  2004-2007 The A2Billing Group
+* @copyright  2004-2008 A2Billing 
 * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
 * @version    CVS: $Id:$
 * @since      File available since Release 1.0
@@ -37,7 +37,7 @@
 * @category   Database
 * @package    Table
 * @author     Arezqui Belaid <areski _atl_ gmail com>
-* @copyright  2004-2007 The A2Billing Group
+* @copyright  2004-2008 A2Billing
 * @license    http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
 * @version    CVS: $Id:$
 * @since      File available since Release 1.0
@@ -51,19 +51,20 @@ class Table {
 	var $table  				= '';
 	var $errstr 				= '';
 	var $debug_st 				= 0;
-	var $debug_stop_add 		= 0;
-	var $debug_stop_update 		= 0;
-	var $debug_stop_delete 		= 0;
-	var $sp                 	= "`"; //bound_caract
+	var $debug_st_stop 			= 0;
 	var $start_message_debug 	= '<table width="100%" align="right" style="float : left;"><tr><td>QUERY:';
 	var $end_message_debug 		= '</td></tr></table><br><br><br>';
+	var $alert_query_time 		= 0.1;
+	var $alert_query_long_time 	= 2;
+	
+	var $writelog 				= false;
 
     var $FK_TABLES;
     var $FK_EDITION_CLAUSE;
     // FALSE if you want to delete the dependent Records, TRUE if you want to update
     // Dependent Records to -1
-    var $FK_DELETE = true;
-    var $FK_ID_VALUE = 0;
+    var $FK_DELETE 				= true;
+    var $FK_ID_VALUE 			= 0;
 
 
 	/* CONSTRUCTOR */
@@ -94,24 +95,54 @@ class Table {
 		$this -> table = $table;
 	}
 
-
-
-	// If $select is not supplied then function check numrows
-	// so expect a SELECT query.
-
-	function SQLExec ($DBHandle, $QUERY, $select = 1, $cache = 0)
+	/*
+	 * ExecuteQuery
+	 */
+	function ExecuteQuery ($DBHandle, $QUERY, $cache = 0)
 	{
+		if ($this -> writelog) {
+			global $A2B;
+			$time_start = microtime(true);
+		}
+		
 		if ($this -> debug_st) echo $this->start_message_debug.$QUERY.$this->end_message_debug;
 		if ($cache > 0) {
 			$res = $DBHandle -> CacheExecute($cache, $QUERY);
 		} else {
 			$res = $DBHandle -> Execute($QUERY);
 		}
-
+		
 		if (!$res) {
-			$this -> errstr = "Could not do a select count on the table '".$this -> table."'";
-			return false;
+			$this -> errstr = $DBHandle -> ErrorMsg();
+			if ($this -> debug_st)
+				echo $DBHandle -> ErrorMsg();
+			if ($this -> debug_st_stop)
+				exit;
 		}
+		
+		$time_end = microtime(true);
+		$time = $time_end - $time_start;
+        
+        if ($this -> writelog) {
+			if ($time > $this->alert_query_time) {
+				if ($time > $this->alert_query_long_time ) 
+					$A2B -> debug( WRITELOG, $agi, __FILE__, __LINE__, "EXTRA_TOOLONG_DB_QUERY - RUNNING TIME = $time");
+				else 
+					$A2B -> debug( WRITELOG, $agi, __FILE__, __LINE__, "TOOLONG_DB_QUERY - RUNNING TIME = $time");
+			}
+			$A2B -> debug( WRITELOG, $agi, __FILE__, __LINE__, "Running time=$time - QUERY=$QUERY");
+        }
+        
+		return $res;
+	}
+
+	// If $select is not supplied then function check numrows
+	// so expect a SELECT query.
+
+	function SQLExec ($DBHandle, $QUERY, $select = 1, $cache = 0)
+	{
+		$res = $this -> ExecuteQuery ($DBHandle, $QUERY, $cache);
+		if (!$res) return false;
 
 		if ($select) {
 			$num = $res -> RecordCount();
@@ -163,21 +194,10 @@ class Table {
 		}
 
 		$QUERY = $sql.$sql_clause.$sql_group.$sql_orderby.$sql_limit;
-		//print $QUERY;
 
-		if ($this -> debug_st) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug;
-		}
-
-		if ($cache > 0) {
-			$res = $DBHandle -> CacheExecute($cache, $QUERY);
-		} else {
-			$res = $DBHandle -> Execute($QUERY);
-		}
-		if (!$res) {
-			$this -> errstr = "Could not do a select on the table '".$this -> table."'";
-			return (false);
-		}
+		$res = $this -> ExecuteQuery ($DBHandle, $QUERY, $cache);
+		if (!$res) return false;
+		
 		$num = $res -> RecordCount();
 		if ($num==0) {
 			return 0;
@@ -186,11 +206,12 @@ class Table {
 		for( $i=0 ; $i<$num ; $i++ ) {
 			$row [] =$res -> fetchRow();
 		}
+		
 		return ($row);
 	}
 
 
-	function Table_count ($DBHandle, $clause=null, $id_Value = null)
+	function Table_count ($DBHandle, $clause=null, $id_Value = null, $cache = 0)
 	{
 		$sql = 'SELECT count(*) FROM '.trim($this -> table);
 
@@ -204,15 +225,10 @@ class Table {
         }
 
 		$QUERY = $sql.$sql_clause;
-		if ($this -> debug_st) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug;
-		}
-		$res = $DBHandle -> Execute($QUERY);
-
-		if (!$res) {
-			$this -> errstr = "Could not do a select count on the table '".$this -> table."'";
-			return (false);
-		}
+		
+		$res = $this -> ExecuteQuery ($DBHandle, $QUERY, $cache);
+		if (!$res) return false;
+		
 		$row =$res -> fetchRow();
 
 		return ($row['0']);
@@ -228,26 +244,14 @@ class Table {
 			$this -> table = $func_table;
 		}
 		if ($subquery) {
-			$QUERY = "INSERT INTO $sp".$this -> table."$sp (".$this -> fields.") (".trim ($value).")";
+			$QUERY = "INSERT INTO ".$this -> table." (".$this -> fields.") (".trim ($value).")";
 		} else {
-			$QUERY = "INSERT INTO $sp".$this -> table."$sp (".$this -> fields.") values (".trim ($value).")";
+			$QUERY = "INSERT INTO ".$this -> table." (".$this -> fields.") values (".trim ($value).")";
 		}
-		if ($this -> debug_st) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug;
-		}
-
-		$res = $DBHandle -> Execute($QUERY);
-
-		if (!$res) {
-			$this -> errstr = $DBHandle->ErrorMsg();
-			if ($this -> debug_st) {
-				echo $DBHandle->ErrorMsg();
-			}
-			return (false);
-		}
-		if ($this -> debug_stop_add) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug; exit();
-		}
+		
+		$res = $this -> ExecuteQuery ($DBHandle, $QUERY, 0);
+		if (!$res) return false;
+		
 		// Fix that , make PEAR complaint
 		if ($id_name!="") {
 
@@ -263,14 +267,16 @@ class Table {
 					return (false);
 				}
 				$row [] =$res -> fetchRow();
-
+				if ($this -> debug_st)
+					echo "\n <br> psql_insert_id = ".$row[0][0];
+				
 				return $row[0][0];
 
 			} else {
 				$insertid = $DBHandle -> Insert_ID();
-				if ($this -> debug_st) {
-					echo "<br>mysql_insert_id :: $insertid";
-				}
+				if ($this -> debug_st)
+					echo "\n <br> mysql_insert_id = $insertid";
+				
 				return $insertid;
 			}
 		}
@@ -279,33 +285,16 @@ class Table {
 	}
 
 
-	function Update_table ($DBHandle, $param_update, $clause, $func_table = null) {
-
-		if ($param_update=="" || $clause=="") {
-			echo "<br>Update parameters wasn't correctly defined.<br>Check the function call 'Update_table'.";
-			return (false);
-		}
-
-		if ($func_table !="") {
+	function Update_table ($DBHandle, $param_update, $clause, $func_table = null)
+	{
+		
+		if ($func_table !="")
 			$this -> table = $func_table;
-		}
-
-		$QUERY = "UPDATE $sp".$this -> table."$sp SET ".trim ($param_update)." WHERE ".trim ($clause);
-		if ($this -> debug_st) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug;
-		}
-		if ($this -> debug_stop_update) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug; exit();
-		}
-		$res = $DBHandle -> Execute($QUERY);
-
-		if (!$res) {
-			$this -> errstr = $DBHandle->ErrorMsg();
-			if ($this -> debug_st) {
-				echo $DBHandle->ErrorMsg();
-			}
-			return (false);
-		}
+		
+		$QUERY = "UPDATE ".$this -> table." SET ".trim ($param_update)." WHERE ".trim ($clause);
+		
+		$res = $this -> ExecuteQuery ($DBHandle, $QUERY, 0);
+		if (!$res) return false;
 
 		return (true);
 	}
@@ -314,43 +303,27 @@ class Table {
 
 	function Delete_table ($DBHandle, $clause, $func_table = null)
 	{
-		if ($clause=="") {
-			echo "<br>Delete parameters wasn't correctly defined.<br>Check the function call 'Update_table'.";
-			return (false);
-		}
-
-		if ($func_table !="") {
+		
+		if ($func_table !="")
 			$this -> table = $func_table;
-		}
-
+		
         $countFK = count($this->FK_TABLES);
         for ($i = 0; $i < $countFK; $i++) {
             if ($this -> FK_DELETE == false) {
-            	$QUERY = "UPDATE $sp".$this -> FK_TABLES[$i]."$sp SET ".
+            	$QUERY = "UPDATE ".$this -> FK_TABLES[$i]." SET ".
 							trim ($this -> FK_EDITION_CLAUSE[$i])." = -1 WHERE (".trim ($this -> FK_EDITION_CLAUSE[$i])." = ".$this -> FK_ID_VALUE." )";
             } else {
-                $QUERY = "DELETE FROM $sp".$this -> FK_TABLES[$i].
-							"$sp WHERE (".trim ($this -> FK_EDITION_CLAUSE[$i])." = ".$this -> FK_ID_VALUE." )";
+                $QUERY = "DELETE FROM ".$this -> FK_TABLES[$i].
+							" WHERE (".trim ($this -> FK_EDITION_CLAUSE[$i])." = ".$this -> FK_ID_VALUE." )";
             }
 			if ($this -> debug_st) echo "<br>$QUERY";
             $res = $DBHandle -> Execute($QUERY);
         }
 
-		$QUERY = "DELETE FROM $sp".$this -> table."$sp WHERE (".trim ($clause).")";
-		if ($this -> debug_st) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug;
-		}
-		if ($this -> debug_stop_delete) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug; exit();
-		}
-		$res = $DBHandle -> Execute($QUERY);
-		if (!$res) {
-			$this -> errstr = $DBHandle->ErrorMsg();
-			if ($this -> debug_st) {
-				echo $DBHandle->ErrorMsg();
-			}
-			return (false);
-		}
+		$QUERY = "DELETE FROM ".$this -> table." WHERE (".trim ($clause).")";
+		$res = $this -> ExecuteQuery ($DBHandle, $QUERY, 0);
+		if (!$res) return false;
+		
 		return (true);
 	}
 
@@ -376,17 +349,13 @@ class Table {
 		}
 
         $QUERY = $sql.$sql_clause;
-		if ($this -> debug_st) {
-			echo $this->start_message_debug.$QUERY.$this->end_message_debug;
-		}
-        $res = $DBHandle -> Execute($QUERY);
-		if (!$res) {
-			$this -> errstr = "Could not do a select on the table '".$this -> table."'";
-			return (false);
-		}
+		
+		$res = $this -> ExecuteQuery ($DBHandle, $QUERY, 0);
+		if (!$res) return false;
+		
 		return (true);
 	}
+	
 
 };
 
-?>
