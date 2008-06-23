ca0a5d77da0fb1f42c442b9176f85ad642fc497f
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-06-23 16:30:42 +0000

    bug on edit key
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@853 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- admin/Public/A2B_entity_config.php
+++ admin/Public/A2B_entity_config.php
@@ -5,7 +5,7 @@ include ("../lib/Form/Class.FormHandler.inc.php");
 include ("./form_data/FG_var_config.inc");
 include ("../lib/admin.smarty.php");
 
-if (! has_rights (ACX_MISC)){
+if (!has_rights (ACX_MISC)){
 	Header ("HTTP/1.0 401 Unauthorized");
 	Header ("Location: PP_error.php?c=accessdenied");
 	die();
