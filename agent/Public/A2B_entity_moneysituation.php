d7a9e08b019f6a7f3424d781237feefc7f58eb2e
rach <rach@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-06-23 14:58:50 +0000

    Correct bug: Refill an payment popup was disabled
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@847 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- agent/Public/A2B_entity_moneysituation.php
+++ agent/Public/A2B_entity_moneysituation.php
@@ -11,7 +11,17 @@ if (! has_rights (ACX_BILLING)){
 	   die();	   
 }
 
+?>
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
 
+//-->
+</script>
+
+<?php
 
 /***********************************************************************************/
 
