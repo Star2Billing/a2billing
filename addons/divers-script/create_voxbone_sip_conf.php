7a0c6a9e715524a0bab6371ff947ad3a3e707cca
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-07-25 15:43:09 +0000

    script to create Voxbone sip.conf
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@897 9d51a8f0-6521-42ea-932e-d822aa02ce2f

---
+++ addons/divers-script/create_voxbone_sip_conf.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php -q
+<?php
+/***************************************************************************
+ * 
+ *            create_voxbone_sip_conf.php
+ *
+ *
+ *  25 July 2008
+ *  Purpose: create sip.conf file to configure VoxBone DID and grant their IPs
+ *
+ *  USAGE : ./create_voxbone_sip_conf.php > voxbone_sip.conf
+ * 
+****************************************************************************/
+exit();
+
+
+// LOAD Voxbone IPs
+$lines = file('voxbone_IPs.txt');
+$context = "a2billing-did";
+
+// Loop through the Master SPID file
+foreach ($lines as $cur_ip) {
+	$cur_ip = trim($cur_ip);
+	echo "[$cur_ip]\nhost = $cur_ip\ntype = friend\ninsecure = very\ncontext = $context\ncanreinvite=no\n\n";
+}
+
+
+
