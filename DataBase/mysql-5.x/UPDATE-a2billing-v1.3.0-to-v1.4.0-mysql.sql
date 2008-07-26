5b1c050875d1e6059b58d31606037b8d77044390
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-07-26 17:34:38 +0000

    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@903 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/mysql-5.x/UPDATE-a2billing-v1.3.0-to-v1.4.0-mysql.sql
+++ DataBase/mysql-5.x/UPDATE-a2billing-v1.3.0-to-v1.4.0-mysql.sql
@@ -601,14 +601,14 @@ ALTER TABLE cc_call ADD COLUMN real_sessiontime INT (11) DEFAULT NULL;
 
 -- Never too late to add some indexes :D
 
-ALTER TABLE `cc_call` ADD INDEX ( `username` );
-ALTER TABLE `cc_call` ADD INDEX ( `starttime` );
-ALTER TABLE `cc_call` ADD INDEX ( `terminatecause` );
-ALTER TABLE `cc_call` ADD INDEX ( `calledstation` );
+ALTER TABLE cc_call ADD INDEX ( `username` );
+ALTER TABLE cc_call ADD INDEX ( `starttime` );
+ALTER TABLE cc_call ADD INDEX ( `terminatecause` );
+ALTER TABLE cc_call ADD INDEX ( `calledstation` );
 
 
-ALTER TABLE `cc_card` ADD INDEX ( `creationdate` );
-ALTER TABLE `cc_card` ADD INDEX ( `username` );
+ALTER TABLE cc_card ADD INDEX ( `creationdate` );
+ALTER TABLE cc_card ADD INDEX ( `username` );
 
 
 
