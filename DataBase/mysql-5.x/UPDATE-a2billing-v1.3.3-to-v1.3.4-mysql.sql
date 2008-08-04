cbe691586092dfe7537c083fa1b5cbacb5e70864
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-08-04 19:40:33 +0000

    add index on 1.3 branch
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@930 9d51a8f0-6521-42ea-932e-d822aa02ce2f

---
+++ DataBase/mysql-5.x/UPDATE-a2billing-v1.3.3-to-v1.3.4-mysql.sql
@@ -0,0 +1,11 @@
+
+-- Never too late to add some indexes :D
+
+ALTER TABLE cc_call ADD INDEX ( `username` );
+ALTER TABLE cc_call ADD INDEX ( `starttime` );
+ALTER TABLE cc_call ADD INDEX ( `terminatecause` );
+ALTER TABLE cc_call ADD INDEX ( `calledstation` );
+
+
+ALTER TABLE cc_card ADD INDEX ( `creationdate` );
+ALTER TABLE cc_card ADD INDEX ( `username` );
---
+++ DataBase/psql/UPDATE-a2billing-v1.3.3-to-v1.3.4-pgsql.sql
@@ -0,0 +1,11 @@
+
+-- Never too late to add some indexes :D
+
+CREATE INDEX cc_call_username_ind ON cc_call USING btree (username);
+CREATE INDEX cc_call_starttime_ind ON cc_call USING btree (starttime);
+CREATE INDEX cc_call_terminatecause_ind ON cc_call USING btree (terminatecause);
+CREATE INDEX cc_call_calledstation_ind ON cc_call USING btree (calledstation);
+
+
+CREATE INDEX cc_card_creationdate_ind ON cc_card USING btree (creationdate);
+CREATE INDEX cc_card_username_ind ON cc_card USING btree (username);
