a8156594624e81a17218d3718f44b3381aed1dfa
rach <rach@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-09-17 16:02:57 +0000

    correct limit behaviours for adding callerid in customer section
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1040 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- customer/A2B_entity_callerid.php
+++ customer/A2B_entity_callerid.php
@@ -28,13 +28,10 @@ if (strlen($add_callerid)>0  && is_numeric($add_callerid)){
 	$instance_sub_table = new Table('cc_callerid');
 	$QUERY = "SELECT count(*) FROM cc_callerid WHERE id_cc_card='".$_SESSION["card_id"]."'";
 	$result = $instance_sub_table -> SQLExec ($HD_Form -> DBHandle, $QUERY, 1);
-	
 	// CHECK IF THE AMOUNT OF CALLERID IS LESS THAN THE LIMIT
 	if ($result[0][0] < $A2B->config["webcustomerui"]['limit_callerid']){
-		
 		$QUERY = "INSERT INTO cc_callerid (id_cc_card, cid) VALUES ('".$_SESSION["card_id"]."', '".$add_callerid."')";
 		$result = $instance_sub_table -> SQLExec ($HD_Form -> DBHandle, $QUERY, 0);
-		
 	}
 }
 /***********************************************************************************/
@@ -56,19 +53,18 @@ $list = $HD_Form -> perform_action($form_action);
 // #### HEADER SECTION
 $smarty->display( 'main.tpl');
 
-
-
 if ($form_action == "list")
 {
     // My code for Creating two functionalities in a page
     $HD_Form -> create_toppage ("ask-add");
     if (strlen($_GET["menu"])>0) $_SESSION["menu"] = $_GET["menu"];
     
-
 	if (isset($update_msg) && strlen($update_msg)>0) echo $update_msg;
+	
+	if(sizeof($list)<$A2B->config["webcustomerui"]['limit_callerid']){
 
 ?>
-	  </center>
+	 
 	   <table align="center"  border="0" width="55%" class="bgcolor_006">
 		<form name="theForm" action="<?php  $_SERVER["PHP_SELF"]?>">
 		<tr class="bgcolor_001" >
@@ -81,10 +77,24 @@ if ($form_action == "list")
 						<input class="form_input_button"  value="<?php echo gettext("ADD NEW CALLERID"); ?>"  type="submit">
 		</td>
         </tr>
-	</form>
+		</form>
       </table>
 	  <br>
 	<?php
+	}else{ 
+	
+	?>
+		<table align="center"  border="0" width="70%" class="bgcolor_006">
+			<tr class="bgcolor_001" >
+				<td align="center" valign="middle">
+					<b><i> <?php  echo gettext("You cannot add more CallerID. Please delete one before");?> </i> </b>
+					<br/>
+					<?php echo gettext("Max CallerId");?> &nbsp;:&nbsp; <?php echo $A2B->config["webcustomerui"]['limit_callerid'] ?>
+	  			</td>
+     		 </tr>
+	 	</table>
+	<?php 	 
+	}
     // END END END My code for Creating two functionalities in a page
 }
 
