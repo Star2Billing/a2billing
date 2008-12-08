4c1b201917daf842166aec4d2c13476484cffcaa
rach <rach@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2008-12-08 18:16:34 +0000

    Add info on calls -amount sell by month -time used by month -amount buy by month
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1329 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- admin/Public/modules/calls_lastmonth.php
+++ admin/Public/modules/calls_lastmonth.php
@@ -17,11 +17,20 @@ $QUERY_GRAPH_CALL_ANSWERED = "SELECT UNIX_TIMESTAMP(DATE_FORMAT(starttime,'%Y-%m
 $result_graph_calls_answered = $table -> SQLExec($DBHandle,$QUERY_GRAPH_CALL_ANSWERED);
 $QUERY_GRAPH_CALL_INCOMPLET = "SELECT UNIX_TIMESTAMP(DATE_FORMAT(starttime,'%Y-%m-01')), count(*) FROM cc_call WHERE starttime>= TIMESTAMP('$checkdate') AND starttime <= CURRENT_TIMESTAMP AND terminatecauseid != 1 GROUP BY MONTH(starttime) ORDER BY starttime;";
 $result_graph_calls_incomplet = $table -> SQLExec($DBHandle,$QUERY_GRAPH_CALL_INCOMPLET);
+$QUERY_GRAPH_CALL_TIMES = "SELECT UNIX_TIMESTAMP(DATE_FORMAT(starttime,'%Y-%m-01')), sum(sessiontime) FROM cc_call WHERE starttime>= TIMESTAMP('$checkdate') AND starttime <= CURRENT_TIMESTAMP GROUP BY MONTH(starttime) ORDER BY starttime;";
+$result_graph_calls_times = $table -> SQLExec($DBHandle,$QUERY_GRAPH_CALL_TIMES);
+$QUERY_GRAPH_CALL_SELL = "SELECT UNIX_TIMESTAMP(DATE_FORMAT(starttime,'%Y-%m-01')), sum(sessionbill) FROM cc_call WHERE starttime>= TIMESTAMP('$checkdate') AND starttime <= CURRENT_TIMESTAMP GROUP BY MONTH(starttime) ORDER BY starttime;";
+$result_graph_calls_sell = $table -> SQLExec($DBHandle,$QUERY_GRAPH_CALL_SELL);
+$QUERY_GRAPH_CALL_BUY = "SELECT UNIX_TIMESTAMP(DATE_FORMAT(starttime,'%Y-%m-01')), sum(buycost) FROM cc_call WHERE starttime>= TIMESTAMP('$checkdate') AND starttime <= CURRENT_TIMESTAMP GROUP BY MONTH(starttime) ORDER BY starttime;";
+$result_graph_calls_buy = $table -> SQLExec($DBHandle,$QUERY_GRAPH_CALL_BUY);
 ?>
 
 
 <input id="call_answer" type="radio" name="mode_call" value="answered">&nbsp; <?php echo gettext("ANSWERED CALL BY MONTH"); ?><br/>
 <input id="call_incomplet" type="radio" name="mode_call" value="incomplet">&nbsp; <?php echo gettext("INCOMPLET CALL BY MONTH"); ?><br/>
+<input id="call_times" type="radio" name="mode_call" value="incomplet">&nbsp; <?php echo gettext("TIMES CALLS BY MONTH"); ?><br/>
+<input id="call_sell" type="radio" name="mode_call" value="incomplet">&nbsp; <?php echo gettext("AMOUNT SELL BY MONTH"); ?><br/>
+<input id="call_buy" type="radio" name="mode_call" value="incomplet">&nbsp; <?php echo gettext("AMOUNT BUY BY MONTH"); ?><br/>
 <br/>
 <div id="call_graph" class="dashgraph" style="width:310px;height:170px;margin-left: auto;margin-right: auto;"></div>
 
@@ -53,13 +62,55 @@ $result_graph_calls_incomplet = $table -> SQLExec($DBHandle,$QUERY_GRAPH_CALL_IN
   	 }
 	 $val_incomplet.="]";
 	 
+	 $max_times=0;
+	 $val_times= "[";
+  	 if(is_array($result_graph_calls_times)){
+		 for($i=0;$i<count($result_graph_calls_times);$i++) {
+		 	$max_times = max($max_times,$result_graph_calls_times[$i][1]);
+		   	$val_times.="[".$result_graph_calls_times[$i][0]."000,".$result_graph_calls_times[$i][1]."]";
+			if($i<count($result_graph_calls_times)-1){ 
+		   	$val_times.=",";
+		   }
+		 }
+  	 }
+	 $val_times.="]";
+	 
+	 
+	 $max_sell=0;
+	 $val_sell= "[";
+  	 if(is_array($result_graph_calls_sell)){
+		 for($i=0;$i<count($result_graph_calls_sell);$i++) {
+		 	$max_sell = max($max_sell,$result_graph_calls_sell[$i][1]);
+		   	$val_sell.="[".$result_graph_calls_sell[$i][0]."000,".$result_graph_calls_sell[$i][1]."]";
+			if($i<count($result_graph_calls_sell)-1){ 
+		   	$val_sell.=",";
+		   }
+		 }
+  	 }
+	 $val_sell.="]";
+	 
+	 $max_buy=0;
+	 $val_buy= "[";
+  	 if(is_array($result_graph_calls_buy)){
+		 for($i=0;$i<count($result_graph_calls_buy);$i++) {
+		 	$max_buy = max($max_buy,$result_graph_calls_buy[$i][1]);
+		   	$val_buy.="[".$result_graph_calls_buy[$i][0]."000,".$result_graph_calls_buy[$i][1]."]";
+			if($i<count($result_graph_calls_buy)-1){ 
+		   	$val_buy.=",";
+		   }
+		 }
+  	 }
+	 $val_buy.="]";
+	 
+	 
 	 
 	 ?>
 <script id="source" language="javascript" type="text/javascript">
-  	
+ 
+var format = "";  	
 $(document).ready(function () {
 	$('#call_answer').click(function () {
-	
+		format ="";
 		var d = <?php echo $val_answer ?>;
     	$.plot($("#call_graph"), [
 						        {
@@ -87,6 +138,7 @@ $(document).ready(function () {
 	
 	        });
 	$('#call_incomplet').click(function () {
+			format ="";
 			var d = <?php echo $val_incomplet ?>;
     		$.plot($("#call_graph"), [
 						        {
@@ -114,7 +166,93 @@ $(document).ready(function () {
 				
 			
 	        });
-	
+	$('#call_sell').click(function () {
+			format ="money";
+			var d = <?php echo $val_sell ?>;
+    		$.plot($("#call_graph"), [
+						        {
+						            data: d,
+						            bars: { show: true,
+						            		barWidth: <?php echo 28*24 * 60 * 60 * 1000; ?>,
+						            		align: "centered"
+						            }
+						        }
+   							 ],
+						    {   xaxis: {
+							    mode: "time",
+						   		timeformat: "%b",
+							    ticks :6,
+						   		min : <?php echo $mingraph."000" ?>,
+						   		max : <?php echo $maxgraph."000" ?>
+							  }, 
+							  yaxis: {
+							  max:<?php echo ($max_sell+5-($max_sell%5)); ?>,
+							  minTickSize: 1,
+							  tickDecimals:0
+							  },selection: { mode: "y" },
+						         grid: { hoverable: true,clickable: true}
+							  });
+				
+			
+	        });
+	$('#call_buy').click(function () {
+			format ="money";
+			var d = <?php echo $val_buy ?>;
+    		$.plot($("#call_graph"), [
+						        {
+						            data: d,
+						            bars: { show: true,
+						            		barWidth: <?php echo 28*24 * 60 * 60 * 1000; ?>,
+						            		align: "centered"
+						            }
+						        }
+   							 ],
+						    {   xaxis: {
+							    mode: "time",
+						   		timeformat: "%b",
+							    ticks :6,
+						   		min : <?php echo $mingraph."000" ?>,
+						   		max : <?php echo $maxgraph."000" ?>
+							  }, 
+							  yaxis: {
+							  max:<?php echo ($max_buy+5-($max_buy%5)); ?>,
+							  minTickSize: 1,
+							  tickDecimals:0
+							  },selection: { mode: "y" },
+						         grid: { hoverable: true,clickable: true}
+							  });
+				
+			
+	        });
+	$('#call_times').click(function () {
+			format ="times";
+			var d = <?php echo $val_times ?>;
+    		$.plot($("#call_graph"), [
+						        {
+						            data: d,
+						            bars: { show: true,
+						            		barWidth: <?php echo 28*24 * 60 * 60 * 1000; ?>,
+						            		align: "centered"
+						            }
+						        }
+   							 ],
+						    {   xaxis: {
+							    mode: "time",
+						   		timeformat: "%b",
+							    ticks :6,
+						   		min : <?php echo $mingraph."000" ?>,
+						   		max : <?php echo $maxgraph."000" ?>
+							  }, 
+							  yaxis: {
+							  max:<?php echo ($max_times+5-($max_times%5)); ?>,
+							  minTickSize: 1,
+							  tickDecimals:0
+							  },selection: { mode: "y" },
+						         grid: { hoverable: true,clickable: true}
+							  });
+				
+			
+	        });
 	        
 	        
  $('#call_answer').click();
@@ -139,9 +277,20 @@ $(document).ready(function () {
                     previousPoint = item.datapoint;
                     
                     $("#tooltip").remove();
-                    var y = item.datapoint[1].toFixed(0);
-                    
-                    showTooltip(item.pageX, item.pageY, y);
+                   
+                    if (format=="time"){
+                    	var y = item.datapoint[1].toFixed(0);
+                    	var hour= Math.floor(y/3600);
+                    	var min= Math.floor(y/60)%60;
+                    	var sec= y%60;
+                    	showTooltip(item.pageX, item.pageY, hour+"h "+min+"m "+sec+"s<br/>("+y+" sec)");
+                    }if(format=="money"){
+                    	 var y = item.datapoint[1].toFixed(2);
+                    	 showTooltip(item.pageX, item.pageY, y+" <?php echo $A2B->config["global"]["base_currency"];?>");
+                    }else{
+                    	var y = item.datapoint[1].toFixed(0);
+                    	showTooltip(item.pageX, item.pageY, y);
+                    }
                 }
             }
             else {
