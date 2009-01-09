fa9f66b7d374addcd93d53c4e80f4b89bcd6cd84
stavros <stavros@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-01-09 01:26:54 +0000

    For some reason the SQL 1.3.0->1.3.1 updates haves been rolled in the 1.3.0 schema.  Not needed here then.
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1371 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/psql/UPDATE-a2billing-v1.3.0-to-v1.3.1-pgsql.sql
+++ DataBase/psql/UPDATE-a2billing-v1.3.0-to-v1.3.1-pgsql.sql
@@ -1,42 +1,8 @@
 
 \set ON_ERROR_STOP ON;
 
-INSERT INTO cc_templatemail VALUES ('epaymentverify', 'info@call-labs.com', 'Call-Labs', 'Epayment Gateway Security Verification Failed', 'Dear Administrator
-
-Please check the Epayment Log, System has logged a Epayment Security failure. that may be a possible attack on epayment processing.
-
-Time of Transaction: $time
-Payment Gateway: $paymentgateway
-Amount: $amount
-
-
-
-Kind regards,
-Call Labs
-', '');
-
--- fix various uses of ISO-3166-1 alpha-2 rather than alpha-3
-UPDATE cc_country SET countrycode='BVT' WHERE countrycode='BV';
-UPDATE cc_country SET countrycode='IOT' WHERE countrycode='IO';
-UPDATE cc_country SET countrycode='HMD' WHERE countrycode='HM';
-UPDATE cc_country SET countrycode='PCN' WHERE countrycode='PN';
-UPDATE cc_country SET countrycode='SGS' WHERE countrycode='GS';
-UPDATE cc_country SET countrycode='SJM' WHERE countrycode='SJ';
-UPDATE cc_country SET countrycode='TLS' WHERE countrycode='TL';
-UPDATE cc_country SET countrycode='UMI' WHERE countrycode='UM';
-UPDATE cc_country SET countrycode='ESH' WHERE countrycode='EH';
 
 -- integrate changes from ISO-3166-1 newsletters V-1 to V-12
 UPDATE cc_country SET countryname='Lao People''s Democratic Republic' WHERE countrycode='LAO';
 UPDATE cc_country SET countryname='Timor-Leste', countryprefix='670' WHERE countrycode='TLS';
 UPDATE cc_country SET countryprefix='0' WHERE countrycode='TMP';
-INSERT INTO cc_country VALUES (246, 'ALA' ,'35818', 'Aland Islands');
-INSERT INTO cc_country VALUES (247, 'BLM' ,'0', 'Saint Barthelemy');
-INSERT INTO cc_country VALUES (248, 'GGY' ,'441481', 'Guernsey');
-INSERT INTO cc_country VALUES (249, 'IMN' ,'441624', 'Isle of Man');
-INSERT INTO cc_country VALUES (250, 'JEY' ,'441534', 'Jersey');
-INSERT INTO cc_country VALUES (251, 'MAF' ,'0', 'Saint Martin');
-INSERT INTO cc_country VALUES (252, 'MNE' ,'382', 'Montenegro, Republic of');
-INSERT INTO cc_country VALUES (253, 'SRB' ,'381', 'Serbia, Republic of');
-INSERT INTO cc_country VALUES (254, 'CPT' ,'0', 'Clipperton Island');
-INSERT INTO cc_country VALUES (255, 'TAA' ,'0', 'Tristan da Cunha');
