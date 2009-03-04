4b7e8166464a55261ba8cd7f41146b6c4bc96807
rach <rach@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-03-04 19:15:27 +0000

    Correct precision
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1530 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/mysql-5.x/UPDATE-a2billing-v1.3.4-to-v1.4.0-mysql.sql
+++ DataBase/mysql-5.x/UPDATE-a2billing-v1.3.4-to-v1.4.0-mysql.sql
@@ -1718,5 +1718,6 @@ CREATE TABLE cc_receipt_item (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-
+ALTER TABLE `cc_logpayment` CHANGE `payment` `payment` DECIMAL( 15, 5 ) NOT NULL;
+ALTER TABLE `cc_logpayment_agent` CHANGE `payment` `payment` DECIMAL( 15, 5 ) NOT NULL;  
 
