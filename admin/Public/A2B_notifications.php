510cc09b183093cb05f86c88a8a90150aec62d93
it46 <it46@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-03-15 15:17:22 +0000

    Cleaning up notification admin UI, added help link and cleanup links to Global Config
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1592 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- admin/Public/A2B_notifications.php
+++ admin/Public/A2B_notifications.php
@@ -13,24 +13,91 @@ if (!has_rights (ACX_CUSTOMER)) {
 // #### HEADER SECTION
 $smarty->display('main.tpl');
 
+echo $CC_help_mail_notifications;
+
 ?>
 
 <table align="center"  class="bgcolor_001" border="0" width="65%">
 	<tr>
 	 	<td>
 		<?php if ( has_rights (ACX_MISC)) {		
-				echo gettext("To enable or disable the module of Notifications for the users follow the link to access to configuration section.");
+				echo gettext("All parameters related to the Notifications module can be set using the Global Config available in System Settings menu.<br/>");
 		?>
-		 		<a href="A2B_entity_config.php?groupselect=3&section=8"><?php echo gettext("Configuration")?></a>
+		 		<a href="A2B_entity_config.php?groupselect=notifications"><?php echo gettext("System Settings - Global Config")?></a>
 		<?php 
 			} else { 
 				
-				echo gettext("You don't have enough right to enable or disable the module of Notifications. Ask your administrator");
+				echo gettext("You don't have enough rights to enable or disable the Notifications modules. Ask your administrator");
 			} 
 		?>
 	 	</td>
 	</tr>
 </table>
+<br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+// Load the list of values in the config table ! key=values_notifications
+$key= "cron_notifications";
+$DBHandle  = DbConnect();
+$instance_config_table = new Table("cc_config", "id, config_value");
+$QUERY = " config_key = '".$key."' ";
+$return = null;
+$return = $instance_config_table -> Get_list($DBHandle, $QUERY, 0);
+$id_config = $return[0]["id"];
+
+if (!is_null($return)&& (!empty($return)>0)) {
+?>
+
+<table align="center"  class="bgcolor_001" border="0" width="65%">
+<tr>
+	<td>
+	<?php
+		if($return[0]["config_value"]) echo gettext("Currently, the cron process of notifications is activated.");
+		else echo gettext("Currently, the cron process of notification is deactivated.");
+		echo '<br/>';
+		echo gettext("Make sure that the cron files are correctly configure in the crontab!");
+		echo '<br/>';
+		
+		if ( has_rights (ACX_MISC)) {
+			echo gettext("Press");
+		    echo ' <a href="A2B_entity_config.php?form_action=ask-edit&id='.$id_config.'">';
+			echo gettext("Modify") ."</a> to change enable or disable periodical notifications";
+		} else {  
+			echo gettext("You don't have enough rights To Enable or Disable the process of notification. Ask your administrator");
+
+		}
+	?>
+ 	</td>
+</tr>
+</table>
+<?php } ?>
+
 <br/>
 
 <?php
@@ -50,15 +117,15 @@ if(!is_null($return)&& (!empty($return)>0) ){
 ?>
 <table align="center"  class="bgcolor_001" border="0" width="65%">
 	<tr>
-		<td width="70%"><?php echo gettext("Possible values to choose from when the user receives a notification");?>
+		<td width="70%"><?php echo gettext("This box shows the possible values to choose from when the user receives a notification");?>
 		<br/><br/>
 		<?php 
 			if ( has_rights (ACX_MISC)) {
-					echo gettext("To modify the list of values that the users can choose from follow the link to access the configuration section.");
-					echo ' <a href="A2B_entity_config.php?form_action=ask-edit&id='.$id_config.'&section=8">';
-					echo gettext("Modify") ."</a>";
+					echo gettext("Press");
+					echo ' <a href="A2B_entity_config.php?form_action=ask-edit&id='.$id_config.'">';
+					echo gettext("Modify") ."</a> to change the values.";
 			} else {
-					echo gettext("You don't have enough right to modify the list of values. Ask your administrator");
+					echo gettext("You don't have enough rights to modify the list of values. Ask your administrator");
 			} ?>
 
 			 	</td>
@@ -79,43 +146,6 @@ if(!is_null($return)&& (!empty($return)>0) ){
 
 <?php
 // Load the list of values in the config table ! key=values_notifications
-$key= "cron_notifications";
-$DBHandle  = DbConnect();
-$instance_config_table = new Table("cc_config", "id, config_value");
-$QUERY = " config_key = '".$key."' ";
-$return = null;
-$return = $instance_config_table -> Get_list($DBHandle, $QUERY, 0);
-$id_config = $return[0]["id"];
-
-if (!is_null($return)&& (!empty($return)>0)) {
-?>
-
-<table align="center"  class="bgcolor_001" border="0" width="65%">
-<tr>
-	<td>
-	<?php
-		if($return[0]["config_value"]) echo gettext(" Actually , the cron process of notification is activated.");
-		else echo gettext(" Actually , the cron process of notification is desactivated.");
-		echo '<br/>';
-		echo gettext(" If the cron process is correctly activated in the crontab ! ");
-		echo '<br/>';
-		
-		if ( has_rights (ACX_MISC)) {
-			echo gettext("To enable or disable the process of configuration follow the link to access the configuration section.");
-		    echo ' <a href="A2B_entity_config.php?form_action=ask-edit&id='.$id_config.'&section=8">';
-			echo gettext("Modify") ."</a>";
-		} else {  
-			echo gettext("You don't have enough right To Enable or Disable the process of notification. Ask your administrator");
-
-		}
-	?>
- 	</td>
-</tr>
-</table>
-<?php } ?>
-<br/>
-<?php
-// Load the list of values in the config table ! key=values_notifications
 $key= "delay_notifications";
 $DBHandle  = DbConnect();
 $instance_config_table = new Table("cc_config", "id, config_value");
@@ -130,7 +160,7 @@ if (!is_null($return)&& (!empty($return)>0)) {
 <tr>
 	<td>
 	<?php
-		$msg= gettext("Actually , the delay of notification is ").$return[0]["config_value"];
+		$msg= gettext("Currently, the periodicity of notification is ").$return[0]["config_value"];
 		if($return[0]["config_value"] == 1) $msg.=gettext(" day");
 		else $msg.=gettext(" days");
 		$msg.='.';
@@ -138,11 +168,11 @@ if (!is_null($return)&& (!empty($return)>0)) {
 
 		echo '<br/>';
 		if ( has_rights (ACX_MISC)) {
-			echo gettext("To modify the delay of the notification follow the link to access the configuration section.");
-		    echo ' <a href="A2B_entity_config.php?form_action=ask-edit&id='.$id_config.'&section=8">';
-			echo gettext("Modify") ."</a>";
+			echo gettext("Press");
+		    echo ' <a href="A2B_entity_config.php?form_action=ask-edit&id='.$id_config.'">';
+			echo gettext("Modify") ."</a> to change the periodicity";
 		} else {
-			echo gettext("You don't have enough right to modify the delay of notification. Ask your administrator");
+			echo gettext("You don't have enough rights to modify the delay of notification. Ask your administrator");
 		} ?>
  	</td>
 </tr>
