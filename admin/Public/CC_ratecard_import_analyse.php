11bce309bb51ab3a92877263d738bd668e392e19
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-03-17 15:52:48 +0000

    reformat
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1624 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- admin/Public/CC_ratecard_import_analyse.php
+++ admin/Public/CC_ratecard_import_analyse.php
@@ -5,217 +5,195 @@ include ("../lib/admin.smarty.php");
 
 set_time_limit(0);
 
-if (! has_rights (ACX_RATECARD)){ 
-	Header ("HTTP/1.0 401 Unauthorized");
-	Header ("Location: PP_error.php?c=accessdenied");	   
-	die();	   
+if (!has_rights(ACX_RATECARD)) {
+	Header("HTTP/1.0 401 Unauthorized");
+	Header("Location: PP_error.php?c=accessdenied");
+	die();
 }
 
-getpost_ifset(array('tariffplan','trunk', 'search_sources', 'task', 'status','currencytype','uploadedfile_name','uploadedfile_name'));
+getpost_ifset(array ('tariffplan', 'trunk', 'search_sources', 'task', 'status', 'currencytype', 'uploadedfile_name', 'uploadedfile_name'));
 
-
-//print_r ($_POST);
-//print_r ($HTTP_POST_FILES);
- 
-
-$tariffplanval= split('-:-', $tariffplan);
-if (!is_numeric($tariffplanval[0])){ 
-	echo gettext("No tariffplan defined !"); 
-	exit();
+$tariffplanval = split('-:-', $tariffplan);
+if (!is_numeric($tariffplanval[0])) {
+	echo gettext("No tariffplan defined !");
+	exit ();
 }
 
-$trunkval= split('-:-', $trunk);
-if (!is_numeric($trunkval[0])){ 
-	echo gettext("No Trunk defined !"); 
-	exit();
+$trunkval = split('-:-', $trunk);
+if (!is_numeric($trunkval[0])) {
+	echo gettext("No Trunk defined !");
+	exit ();
 }
 
-if ($search_sources!='nochange'){
-
-	//echo "<br>---$search_sources";
-	$fieldtoimport= split("\t", $search_sources);
+if ($search_sources != 'nochange') {
+	$fieldtoimport = split("\t", $search_sources);
 	$fieldtoimport_sql = str_replace("\t", ", ", $search_sources);
-	$fieldtoimport_sql = trim ($fieldtoimport_sql);
-	if (strlen($fieldtoimport_sql)>0) $fieldtoimport_sql = ', '.$fieldtoimport_sql;
+	$fieldtoimport_sql = trim($fieldtoimport_sql);
+	if (strlen($fieldtoimport_sql) > 0)
+		$fieldtoimport_sql = ', ' . $fieldtoimport_sql;
 }
 
-//echo "<br>---$fieldtoimport_sql<br>";
-//print_r($fieldtoimport);
+$fixfield[0] = "IDTariffplan (KEY)";
+$fixfield[1] = "Outbound Trunk";
 
-
-$fixfield[0]="IDTariffplan (KEY)";
-$fixfield[1]="Outbound Trunk";
-
-$field[0]="Dialprefix";
-$field[1]="Destination Country";
-$field[2]="Rate Initial";
+$field[0] = "Dialprefix";
+$field[1] = "Destination Country";
+$field[2] = "Rate Initial";
 
 $FG_DEBUG = 0;
 
 // THIS VARIABLE DEFINE THE COLOR OF THE HEAD TABLE
 $FG_TABLE_ALTERNATE_ROW_COLOR[] = "#FFFFFF";
 $FG_TABLE_ALTERNATE_ROW_COLOR[] = "#F2F8FF";
-$Temps1 = time();
-if ($FG_DEBUG == 1) echo "::::>> ".$the_file;
-//INUTILE
+
 $my_max_file_size = (int) MY_MAX_FILE_SIZE_IMPORT;
 
-if ($FG_DEBUG == 1) echo "<br> Task :: $task";
-if ($task=='upload'){
+if ($FG_DEBUG == 1)
+	echo "<br> Task :: $task";
 
-	//---------------------------------------------------------
-	//		 Effacer tout les fichiers du repertoire cache.
-	//---------------------------------------------------------
-
+if ($task == 'upload') {
 	$the_file_name = $_FILES['the_file']['name'];
 	$the_file_type = $_FILES['the_file']['type'];
 	$the_file = $_FILES['the_file']['tmp_name'];
 
-	if(count($_FILES) > 0)
-	{
-		$errortext = validate_upload($the_file, $the_file_type);	
-		if ($errortext != "" || $errortext  != false)	
-		{
+	if (count($_FILES) > 0) {
+		$errortext = validate_upload($the_file, $the_file_type);
+		if ($errortext != "" || $errortext != false) {
 			echo $errortext;
 			exit;
 		}
-		$new_filename = "/tmp/".MDP(6).".csv";
-		if (file_exists($new_filename))
-		{
+		$new_filename = "/tmp/" . MDP(6) . ".csv";
+		if (file_exists($new_filename)) {
 			echo $_FILES["file"]["name"] . " already exists. ";
-		}
-		else
-		{
-			if(!move_uploaded_file($_FILES["the_file"]["tmp_name"],	$new_filename))
-			{
-			    echo gettext("File Save Failed, FILE=".$new_filename);
+		} else {
+			if (!move_uploaded_file($_FILES["the_file"]["tmp_name"], $new_filename)) {
+				echo gettext("File Save Failed, FILE=" . $new_filename);
 			}
 		}
 		$the_file = $new_filename;
-	}
-	else
-	{		
+	} else {
 		$the_file_type = $uploadedfile_type;
 		$the_file = $uploadedfile_name;
 	}
 	
-	if ($FG_DEBUG == 1) echo "<br> FILE  ::> ".$the_file_name;
-	if ($FG_DEBUG == 1) echo "<br> THE_FILE:$the_file <br>THE_FILE_TYPE:$the_file_type";	
+	if ($FG_DEBUG == 1)
+		echo "<br> THE_FILE:$the_file <br>THE_FILE_TYPE:$the_file_type";
 	
-	$fp = fopen($the_file,  "r");  
-	if (!$fp){  /* THE FILE DOESN'T EXIST */ 
-		echo  gettext('Error: Failed to open the file.'); 
-		exit(); 
-	} 
-	
-	$nb_imported=0;
-	$nb_to_import=0;
-	$DBHandle  = DbConnect();
-    
-	while (!feof($fp)){ 
-		
+	$fp = fopen($the_file, "r");
+	if (!$fp) {
+		echo gettext('Error: Failed to open the file.');
+		exit ();
+	}
+
+	$nb_imported = 0;
+	$nb_to_import = 0;
+	$DBHandle = DbConnect();
+
+	while (!feof($fp)) {
+
 		//if ($nb_imported==1000) break;
-		$ligneoriginal = fgets($fp,4096);  /* On se dplace d'une ligne */   
-		$ligneoriginal = trim ($ligneoriginal);
-		
+		$ligneoriginal = fgets($fp, 4096); /* On se dplace d'une ligne */
+		$ligneoriginal = trim($ligneoriginal);
+
 		// strip out ' and " and, with the exception of dialprefix field,
 		// substitute , for . to allow European style floats, eg: 0,1 == 0.1
-		$ligne = str_replace(array('"',"'"), '', $ligneoriginal);
-		$val=split('[;:]', $ligne);
+		$ligne = str_replace(array (
+			'"',
+			"'"
+		), '', $ligneoriginal);
+		$val = split('[;:]', $ligne);
 		for ($i = 1; $i < count($val); $i++)
-			 $val[$i]=str_replace(',', '.', $val[$i]);
-		
-		if ($status!="ok"){ 
-			if($currencytype == "cent")
-			{
+			$val[$i] = str_replace(',', '.', $val[$i]);
+
+		if ($status != "ok") {
+			if ($currencytype == "cent") {
 				$val[2] = $val[2] / 100;
 			}
 			break;
 		}
-		if (substr($ligne,0,1)!='#' && $val[2]!='' && strlen($val[2])>0){
-			
-			$FG_ADITION_SECOND_ADD_TABLE  = 'cc_ratecard';		
+		if (substr($ligne, 0, 1) != '#' && $val[2] != '' && strlen($val[2]) > 0) {
+
+			$FG_ADITION_SECOND_ADD_TABLE = 'cc_ratecard';
 			$FG_ADITION_SECOND_ADD_FIELDS = 'idtariffplan, id_trunk, dialprefix, destination, rateinitial'; //$fieldtoimport_sql				
 			if ($currencytype == "cent") {
 				$val[2] = $val[2] / 100;
 			}
-			
-			$FG_ADITION_SECOND_ADD_VALUE  = "'".$tariffplanval[0]."', '".$trunkval[0]."', '".$val[0]."', '".$val[1]."', '".$val[2]."'";
-			
-			for ($k=0;$k<count($fieldtoimport);$k++){
-				
-				if (!empty($val[$k+3]) || $val[$k+3]=='0')
-				{
-					if ($fieldtoimport[$k]=="startdate" && ($val[$k+3]=='0' || $val[$k+3]=='')) continue;
-					if ($fieldtoimport[$k]=="stopdate" && ($val[$k+3]=='0' || $val[$k+3]=='')) continue;
-					
-					if ($fieldtoimport[$k]=="buyrate" || $fieldtoimport[$k]=="connectcharge" || $fieldtoimport[$k]=="disconnectcharge" )
-					{
-						if($currencytype == "cent")
-						{
-							$val[$k+3] = $val[$k+3] / 100;
+
+			$FG_ADITION_SECOND_ADD_VALUE = "'" . $tariffplanval[0] . "', '" . $trunkval[0] . "', '" . $val[0] . "', '" . $val[1] . "', '" . $val[2] . "'";
+
+			for ($k = 0; $k < count($fieldtoimport); $k++) {
+
+				if (!empty ($val[$k +3]) || $val[$k +3] == '0') {
+					if ($fieldtoimport[$k] == "startdate" && ($val[$k +3] == '0' || $val[$k +3] == ''))
+						continue;
+					if ($fieldtoimport[$k] == "stopdate" && ($val[$k +3] == '0' || $val[$k +3] == ''))
+						continue;
+
+					if ($fieldtoimport[$k] == "buyrate" || $fieldtoimport[$k] == "connectcharge" || $fieldtoimport[$k] == "disconnectcharge") {
+						if ($currencytype == "cent") {
+							$val[$k +3] = $val[$k +3] / 100;
 						}
-						
-					} 
-					$FG_ADITION_SECOND_ADD_FIELDS .= ', '.$fieldtoimport[$k];
-					
-					if (is_numeric($val[$k+3])) {
-						$FG_ADITION_SECOND_ADD_VALUE .= ", ".$val[$k+3]."";
-					}else{
-						$FG_ADITION_SECOND_ADD_VALUE .= ", '".trim($val[$k+3])."'";
+
 					}
-					
-					if ($fieldtoimport[$k]=="startdate") $find_stardate = 1;
-					if ($fieldtoimport[$k]=="stopdate") $find_stopdate = 1;
+					$FG_ADITION_SECOND_ADD_FIELDS .= ', ' . $fieldtoimport[$k];
+
+					if (is_numeric($val[$k +3])) {
+						$FG_ADITION_SECOND_ADD_VALUE .= ", " . $val[$k +3] . "";
+					} else {
+						$FG_ADITION_SECOND_ADD_VALUE .= ", '" . trim($val[$k +3]) . "'";
+					}
+
+					if ($fieldtoimport[$k] == "startdate")
+						$find_stardate = 1;
+					if ($fieldtoimport[$k] == "stopdate")
+						$find_stopdate = 1;
 				}
 			}
-			 
-			if ($find_stardate!=1) {
+
+			if ($find_stardate != 1) {
 				$begin_date = date("Y");
 				$end_date = date("-m-d H:i:s");
 				$FG_ADITION_SECOND_ADD_FIELDS .= ', startdate';
-				$FG_ADITION_SECOND_ADD_VALUE .= ", '".$begin_date.$end_date."'";
+				$FG_ADITION_SECOND_ADD_VALUE .= ", '" . $begin_date . $end_date . "'";
 			}
-			 
-			if ($find_stopdate!=1){
-				$begin_date_plus = date("Y")+10;
+
+			if ($find_stopdate != 1) {
+				$begin_date_plus = date("Y") + 10;
 				$end_date = date("-m-d H:i:s");
 				$FG_ADITION_SECOND_ADD_FIELDS .= ', stopdate';
-				$FG_ADITION_SECOND_ADD_VALUE .= ", '".$begin_date_plus.$end_date."'";
+				$FG_ADITION_SECOND_ADD_VALUE .= ", '" . $begin_date_plus . $end_date . "'";
 			}
-				
-			$TT_QUERY .= "INSERT INTO ".$FG_ADITION_SECOND_ADD_TABLE." (".$FG_ADITION_SECOND_ADD_FIELDS.") values (".$FG_ADITION_SECOND_ADD_VALUE.") ";
-			
+
+			$TT_QUERY .= "INSERT INTO " . $FG_ADITION_SECOND_ADD_TABLE . " (" . $FG_ADITION_SECOND_ADD_FIELDS . ") values (" . $FG_ADITION_SECOND_ADD_VALUE . ") ";
+
 			$nb_to_import++;
 		}
-		
-		if ($TT_QUERY!='' && strlen($TT_QUERY)>0 && ($nb_to_import==1) ){
-			
-			$nb_to_import=0;
-			$result_query =  $DBHandle -> Execute($TT_QUERY);
-			
-			if ($result_query){ 
-				$nb_imported = $nb_imported + 1;
-			}else{$buffer_error.= $ligneoriginal.'<br/>';}
-			$TT_QUERY='';
+
+		if ($TT_QUERY != '' && strlen($TT_QUERY) > 0 && ($nb_to_import == 1)) {
+
+			$nb_to_import = 0;
+			$result_query = $DBHandle->Execute($TT_QUERY);
+
+			if ($result_query) {
+				$nb_imported = $nb_imported +1;
+			} else {
+				$buffer_error .= $ligneoriginal . '<br/>';
+			}
+			$TT_QUERY = '';
 		}
-		
+
 	} // END WHILE EOF
-	
-	
-	if ($TT_QUERY!='' && strlen($TT_QUERY)>0 && ($nb_to_import>0) ){
-		$result_query = @ $DBHandle -> Execute($TT_QUERY);
-		if ($result_query) $nb_imported = $nb_imported + $nb_to_import;
+
+	if ($TT_QUERY != '' && strlen($TT_QUERY) > 0 && ($nb_to_import > 0)) {
+		$result_query = @ $DBHandle->Execute($TT_QUERY);
+		if ($result_query)
+			$nb_imported = $nb_imported + $nb_to_import;
 	}
 }
 
-$Temps2 = time();
-$Temps = $Temps2 - $Temps1;
-//echo "<br>".$Temps2;
-//echo "<br>Script Time :".$Temps."<br>";
-
-
 $smarty->display('main.tpl');
+
+
 
 ?>
 
@@ -378,10 +356,9 @@ if ($status=="ok"){
 	<br>
 	
 <?php
-if($uploadedfile_name != "")
-{
+
+if($uploadedfile_name != "") {
 	unlink($uploadedfile_name);
 }
 $smarty->display('footer.tpl');
 
-?>
