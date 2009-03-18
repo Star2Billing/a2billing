1b00d440fc12bb4e6b6bd7515b3c11390406fc2d
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-03-18 17:18:13 +0000

    reformat
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1650 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- admin/Public/A2B_entity_statuslog.php
+++ admin/Public/A2B_entity_statuslog.php
@@ -5,20 +5,19 @@ include ("../lib/Form/Class.FormHandler.inc.php");
 include ("./form_data/FG_var_statuslog.inc");
 include ("../lib/admin.smarty.php");
 
-if (! has_rights (ACX_CUSTOMER)){ 
+if (! has_rights (ACX_CUSTOMER)) {
 	Header ("HTTP/1.0 401 Unauthorized");
 	Header ("Location: PP_error.php?c=accessdenied");	   
 	die();
 }
 
-/***********************************************************************************/
 getpost_ifset(array( 'id_cc_card'));
 
 
 $HD_Form -> setDBHandler (DbConnect());
-
 $HD_Form -> init();
-if ($id!="" || !is_null($id)){
+
+if ($id!="" || !is_null($id)) {
 	$HD_Form -> FG_EDITION_CLAUSE = str_replace("%id", "$id", $HD_Form -> FG_EDITION_CLAUSE);
 }
 
@@ -27,18 +26,12 @@ if (!isset($action)) $action = $form_action;
 
 $list = $HD_Form -> perform_action($form_action);
 
-// #### HEADER SECTION
 $smarty->display('main.tpl');
 
-// #### HELP SECTION
 echo $CC_help_status_log;
 
-// #### TOP SECTION PAGE
 $HD_Form -> create_toppage ($form_action);
 
-
-// #### CREATE FORM OR LIST
-//$HD_Form -> CV_TOPVIEWER = "menu";
 if (strlen($_GET["menu"])>0) $_SESSION["menu"] = $_GET["menu"];
 
 ?>
@@ -202,7 +195,6 @@ if (strlen($_GET["menu"])>0) $_SESSION["menu"] = $_GET["menu"];
 
 $HD_Form -> create_form ($form_action, $list, $id=null) ;
 
-// #### FOOTER SECTION
 $smarty->display('footer.tpl');
 
-?>
+
