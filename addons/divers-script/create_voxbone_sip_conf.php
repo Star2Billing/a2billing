c24ae2ca813d2cdff33c10433fd374fdd998e50a
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-03-26 18:42:16 +0000

    move wholesale_extensions.conf
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1727 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- addons/divers-script/create_voxbone_sip_conf.php
+++ addons/divers-script/create_voxbone_sip_conf.php
@@ -11,14 +11,13 @@
  *  USAGE : ./create_voxbone_sip_conf.php > voxbone_sip.conf
  * 
 ****************************************************************************/
-exit();
 
 
 // LOAD Voxbone IPs
 $lines = file('voxbone_IPs.txt');
 $context = "a2billing-did";
 
-// Loop through the Master SPID file
+// Loop through the voxbone_IPs file
 foreach ($lines as $cur_ip) {
 	$cur_ip = trim($cur_ip);
 	echo "[$cur_ip]\nhost = $cur_ip\ntype = friend\ninsecure = very\ncontext = $context\ncanreinvite=no\n\n";
