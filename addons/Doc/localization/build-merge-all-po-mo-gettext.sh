cf187517b7be0983a311d00984430c32066038eb
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-04-09 14:14:06 +0000

    internazionalization : add shell script to create & generate PO/MO
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1876 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- addons/Doc/localization/build-merge-all-po-mo-gettext.sh
+++ addons/Doc/localization/build-merge-all-po-mo-gettext.sh
@@ -25,49 +25,97 @@ show_menu(){
 	read OPTION < /dev/tty
 }
 
+generate_admin_PO(){
+	clear
+	echo " **** Generate PO for Customer UI *** "
+	
+	DIR_CURRENT=$DIR_ADMIN
+	PO_DIRECTORY_CURRENT=$PO_DIRECTORY_ADMIN
+	
+	cd $cwd
+	cd $DIR_CURRENT
+	
+	rm -rf templates_c/*
+    find -follow | grep -i [.][pit][hnp][pcl]$  | grep -v "customer.*" | grep -v "agent.*" | xargs xgettext -n --no-wrap
+    
+    cd $cwd
+    
+    generate_customer_PO_MO $DIR_CURRENT $PO_DIRECTORY_CURRENT
+    
+}
+
+generate_agent_PO(){
+	clear
+	echo " **** Generate PO for Customer UI *** "
+	
+	DIR_CURRENT=$DIR_AGENT
+	PO_DIRECTORY_CURRENT=$PO_DIRECTORY_AGENT
+	
+	cd $cwd
+	cd $DIR_CURRENT
+	
+	rm -rf templates_c/*
+    find -follow | grep -i [.][pit][hnp][pcl]$  | grep -v "customer.*" | grep -v "admin.*" | xargs xgettext -n --no-wrap
+    
+    cd $cwd
+    
+    generate_customer_PO_MO $DIR_CURRENT $PO_DIRECTORY_CURRENT
+    
+}
+
 generate_customer_PO(){
 	clear
 	echo " **** Generate PO for Customer UI *** "
 	
+	DIR_CURRENT=$DIR_CUSTOMER
+	PO_DIRECTORY_CURRENT=$PO_DIRECTORY_CUSTOMER
+	
 	cd $cwd
-	cd $DIR_CUSTOMER
+	cd $DIR_CURRENT
 	
 	rm -rf templates_c/*
     find -follow | grep -i [.][pit][hnp][pcl]$  | grep -v "admin.*" | grep -v "agent.*" | xargs xgettext -n --no-wrap
     
     cd $cwd
     
+    generate_customer_PO_MO $DIR_CURRENT $PO_DIRECTORY_CURRENT
     
-    # 
-	dirfile=`ls $PO_DIRECTORY_CUSTOMER`
+}
+
+generate_customer_PO_MO(){
+	
+	DIR_CURRENT=$1
+	PO_DIRECTORY_CURRENT=$2
+	
+	dirfile=`ls $PO_DIRECTORY_CURRENT`
 	# dirfile='zh_TW'
 	
     for d in $dirfile
 	do
 		echo "Proceed for the directory : $d"
-		if [ ! -d $PO_DIRECTORY_CUSTOMER/$d ];
+		if [ ! -d $PO_DIRECTORY_CURRENT/$d ];
 		then
-			echo "Error : $PO_DIRECTORY_CUSTOMER/$d directory does not exits!"
+			echo "Error : $PO_DIRECTORY_CURRENT/$d directory does not exits!"
 			s=1
 		fi
 		# move old messages before merging
-		mv $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/messages.po $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/old_messages.po
-		cp $DIR_CUSTOMER/messages.po $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/messages.po
+		mv $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/messages.po $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/old_messages.po
+		cp $DIR_CURRENT/messages.po $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/messages.po
 		
 		# merging new PO with the old one
-		msgmerge --no-wrap $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/old_messages.po $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/messages.po --output-file=$PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/new.po
+		msgmerge --no-wrap $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/old_messages.po $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/messages.po --output-file=$PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/new.po
 		# remove old and use the new
-		rm $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/old_messages.po
-		mv $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/new.po $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/messages.po
+		rm $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/old_messages.po
+		mv $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/new.po $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/messages.po
 		
 		# generate mo
-		msgfmt $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/messages.po -o $PO_DIRECTORY_CUSTOMER/$d/LC_MESSAGES/messages.mo
+		msgfmt $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/messages.po -o $PO_DIRECTORY_CURRENT/$d/LC_MESSAGES/messages.mo
 		
 	done
 	echo ""
 
-	rm $DIR_CUSTOMER/messages.po
-    
+	rm $DIR_CURRENT/messages.po
+	
 }
 
 clear
@@ -76,14 +124,17 @@ echo " **** Asterisk2Billing : Autogenerate gettext & merge ***"
 
 ExitFinish=0
 
-generate_customer_PO
 while [ $ExitFinish -eq 0 ]; do
 
 	# Show menu with Installation items
 	show_menu
 
 	case $OPTION in
+		1) generate_admin_PO
+		;;
 		2) generate_customer_PO
+		;;
+		3) generate_agent_PO
 		;;
 		4)
 		ExitFinish=1
