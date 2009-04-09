1658542c5b2b096e5e87235fe3cc0a93c13cf0ba
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-04-09 16:27:50 +0000

    internazionalization : add brazilian for agent + update french with ### for test
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1888 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- common/lib/agent.defines.php
+++ common/lib/agent.defines.php
@@ -191,7 +191,7 @@ if (isset($language)) {
 }
 
 define ("LANGUAGE",$_SESSION["language"]);
-define ("BINDTEXTDOMAIN", '../../common/agent_ui_locale');
+define ("BINDTEXTDOMAIN", '../common/agent_ui_locale');
 require("languageSettings.php");
 SetLocalLanguage();
 
