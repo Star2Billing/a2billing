399ede98b797dd80b9d7156284371286ca0346ee
rach <rach@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-04-20 12:42:40 +0000

    Change the forward after a payment
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@1938 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- agent/Public/checkout_success.php
+++ agent/Public/checkout_success.php
@@ -62,7 +62,7 @@ $smarty->display( 'main.tpl');
     <td>&nbsp;</td>
 </tr>
 <tr>
-    <td align=center colspan=2><a href="<?php echo tep_href_link("userinfo.php","", 'SSL', false, false);?>">[Home]</a></td>
+    <td align=center colspan=2><a href="<?php echo tep_href_link("agentinfo.php","", 'SSL', false, false);?>">[Home]</a></td>
 </tr>
 
 </table>
