f2fd2d6ed3f8d1f6d86ef4deb161f248f3b1fb53
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-05-20 16:28:05 +0000

    bug on payment agent
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2064 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- common/lib/epayment/classes/order.php
+++ common/lib/epayment/classes/order.php
@@ -22,9 +22,9 @@
         global $languages_id;
 	
         if(isset($_SESSION["agent_id"]) && !empty($_SESSION["agent_id"])){
-        	$QUERY = "SELECT  login as username, credit, lastname, firstname, address, city, state, country, zipcode, phone, email, fax, active as status, currency FROM cc_agent WHERE id = '".$_SESSION["agent_id"]."'";
+        	$QUERY = "SELECT login as username, credit, lastname, firstname, address, city, state, country, zipcode, phone, email, fax, status, currency FROM cc_agent WHERE id = '".$_SESSION["agent_id"]."'";
         }elseif(isset($_SESSION["card_id"]) && !empty($_SESSION["card_id"])){
-        	$QUERY = "SELECT  username, credit, lastname, firstname, address, city, state, country, zipcode, phone, email, fax, status, currency FROM cc_card WHERE id = '".$_SESSION["card_id"]."'";
+        	$QUERY = "SELECT username, credit, lastname, firstname, address, city, state, country, zipcode, phone, email, fax, status, currency FROM cc_card WHERE id = '".$_SESSION["card_id"]."'";
         }else{
         	echo "ERROR";
         	die();
@@ -35,7 +35,7 @@
         $numrow = $resmax -> numRows();
         if ($numrow == 0) {exit();}
         $customer_info =$resmax -> fetchRow();
-        if($customer_info [12] != "1" ) {
+        if ($customer_info [12] != "1" ) {
 			exit();
 		}
 
