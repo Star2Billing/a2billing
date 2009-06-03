827e7f0f2c75646e385b7e61c1ba5d3e6f6af483
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-06-03 16:22:51 +0000

    change parameters dotest + reindent
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2080 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- customer/signup.php
+++ customer/signup.php
@@ -7,70 +7,70 @@ include ("./lib/customer.smarty.php");
 
 
 
+if (!$A2B->config["signup"]['enable_signup'])	
+	exit;
 
-/***********************************************************************************/
-if (!$A2B->config["signup"]['enable_signup']) exit;
+getpost_ifset(array (
+	'test',
+	'subscriber'
+));
 
-getpost_ifset(array('dotest','subscriber'));
 
-if(!is_numeric($subscriber)){
+if (!is_numeric($subscriber)) {
 	//check subscriber
 	$table_check_subscriber = new Table("cc_subscription_signup", "COUNT(*)");
 	$clause_check_subscriber = "";
-	$result_check_subscriber = $table_check_subscriber -> Get_list(DbConnect(),$clause_check_subscriber);
+	$result_check_subscriber = $table_check_subscriber->Get_list(DbConnect(), $clause_check_subscriber);
 	$check_subscriber = $result_check_subscriber[0][0];
-	if($check_subscriber>0){
-		if ($dotest) Header ("Location: signup_service.php?dotest=$dotest");
-		else Header ("Location: signup_service.php");
+	if ($check_subscriber > 0) {
+		if ($dotest)
+			Header("Location: signup_service.php?dotest=$dotest");
+		else
+			Header("Location: signup_service.php");
 		die();
 	}
 }
 
-
-
-if ($dotest) {
-	$_POST["lastname"] = $_POST["firstname"] = $_POST["address"] = $_POST["city"] = $_POST["state"] = $_POST["country"] = 'SIGN-'.MDP_STRING(5).'-'.MDP_NUMERIC(3);
-	$_POST["email"] = MDP_STRING(10).'@sign-up.com';
+if ($test) {
+	$_POST["lastname"] = $_POST["firstname"] = $_POST["address"] = $_POST["city"] = $_POST["state"] = $_POST["country"] = 'SIGN-' . MDP_STRING(5) . '-' . MDP_NUMERIC(3);
+	$_POST["email"] = MDP_STRING(10) . '@sign-up.com';
 	$_POST["zipcode"] = $_POST["phone"] = '12345667789';
 }
 
-$HD_Form -> setDBHandler (DbConnect());
-$HD_Form -> init();
+$HD_Form->setDBHandler(DbConnect());
+$HD_Form->init();
 
-if ($id!="" || !is_null($id)) {
-	$HD_Form -> FG_EDITION_CLAUSE = str_replace("%id", "$id", $HD_Form -> FG_EDITION_CLAUSE);
+if ($id != "" || !is_null($id)) {
+	$HD_Form->FG_EDITION_CLAUSE = str_replace("%id", "$id", $HD_Form->FG_EDITION_CLAUSE);
 }
 
-if (!isset($form_action))  $form_action="ask-add";
-if (!isset($action)) $action = $form_action;
+if (!isset ($form_action))
+	$form_action = "ask-add";
+if (!isset ($action))
+	$action = $form_action;
 
+$list = $HD_Form->perform_action($form_action);
 
-$list = $HD_Form -> perform_action($form_action);
-
-if($form_action == "add") {
-	unset($_SESSION["cardnumber_signup"]);
+if ($form_action == "add") {
+	unset ($_SESSION["cardnumber_signup"]);
 	$_SESSION["language_code"] = $_POST["language"];
-	$_SESSION["cardnumber_signup"] = $maxi;	
-    Header ("Location: signup_confirmation.php");
+	$_SESSION["cardnumber_signup"] = $maxi;
+	Header("Location: signup_confirmation.php");
 }
-
 
 // #### HEADER SECTION
 $smarty->display('signup_header.tpl');
 
-
 // #### TOP SECTION PAGE
-$HD_Form -> create_toppage ($form_action);
-
+$HD_Form->create_toppage($form_action);
 
 // #### CREATE FORM OR LIST
-if (strlen($_GET["menu"])>0) $_SESSION["menu"] = $_GET["menu"];
+if (strlen($_GET["menu"]) > 0)
+	$_SESSION["menu"] = $_GET["menu"];
 
-$HD_Form -> create_form ($form_action, $list, $id=null);
-
+$HD_Form->create_form($form_action, $list, $id = null);
 
 // #### FOOTER SECTION
 $smarty->display('signup_footer.tpl');
-
 
 
