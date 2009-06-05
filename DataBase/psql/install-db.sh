7ac1b422bea339787418ee32ad7b367a90ca8d03
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-06-05 17:13:35 +0000

    Update install DB script
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2098 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/psql/install-db.sh
+++ DataBase/psql/install-db.sh
@@ -21,7 +21,8 @@ read password
 	( echo "$hostname:5432:$dbname:$username:$password" > ~/.pgpass && chmod 0600 ~/.pgpass )
 
 psql -h $hostname -U $username -d $dbname -f a2billing-schema-v1.4.0.sql && \
-	psql -h $hostname -U $username -d $dbname -f a2billing-prefix-table-v1.4.0.sql && exit 0
+psql -h $hostname -U $username -d $dbname -f UPDATE-a2billing-v1.4.0-to-v1.4.1.sql && \
+psql -h $hostname -U $username -d $dbname -f a2billing-prefix-table-v1.4.0.sql && exit 0
 
 ## If we get this far,  the psql commands failed.
 echo "Installation failed."
