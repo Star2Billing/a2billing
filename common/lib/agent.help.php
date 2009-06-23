e46cdbc79ac55b635f1750b3f5c61ae9a405778c
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-06-23 17:32:14 +0000

    fix indents on help files
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2134 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- common/lib/agent.help.php
+++ common/lib/agent.help.php
@@ -1,83 +1,81 @@
 <?php
 
-function help_wiki_link($wiki_article_name)
-{
-	return gettext("For further information please consult").' <a target="_blank" href="http://trac.asterisk2billing.org/cgi-bin/trac.cgi/wiki/1-4-'.$wiki_article_name.'">'.gettext("the online documention").'</a>.<br/>';
+function help_wiki_link($wiki_article_name) {
+	return gettext("For further information please consult") . ' <a target="_blank" href="http://trac.asterisk2billing.org/cgi-bin/trac.cgi/wiki/1-4-' . $wiki_article_name . '">' . gettext("the online documention") . '</a>.<br/>';
 }
 
-
-function create_help($text,$wiki=null) {
-	if(!empty($wiki)) $wiki_text =  help_wiki_link($wiki);
-	else $wiki_text = "";
-	$help='
+function create_help($text, $wiki = null) {
+	if (!empty ($wiki))
+		$wiki_text = help_wiki_link($wiki);
+	else
+		$wiki_text = "";
+	$help = '
 	<div class="toggle_show2hide">
 	<div class="tohide" style="display:visible;">
-	<div class="msg_info">'.$text.'
-	<br/>'.$wiki_text.'<a href="#" target="_self" class="hide_help" style="float:right;"><img class="toggle_show2hide" src="'.KICON_PATH.'/toggle_hide2show_on.png" onmouseover="this.style.cursor=\'hand\';" HEIGHT="16"> </a>
+	<div class="msg_info">' . $text . '
+	<br/>' . $wiki_text . '<a href="#" target="_self" class="hide_help" style="float:right;"><img class="toggle_show2hide" src="' . KICON_PATH . '/toggle_hide2show_on.png" onmouseover="this.style.cursor=\'hand\';" HEIGHT="16"> </a>
 	</div></div></div>';
 	return $help;
-	
-}
 
+}
 
 if (SHOW_HELP) {
 
-$CC_help_agent =create_help(gettext("Agent list who have access to the Agent interface."),'ShowAgent');
+	$CC_help_agent = create_help(gettext("Agent list who have access to the Agent interface."), 'ShowAgent');
 
-$CC_help_agent_info=create_help(gettext("Personal information.").'<br>
-'.gettext("You can update your personal information here."));
+	$CC_help_agent_info = create_help(gettext("Personal information.") . '<br>' . gettext("You can update your personal information here."));
 
-$CC_help_sipfriend_list =create_help(gettext("Voip Config will create a SIP or IAX entry on the Asterisk server, so that a customer can set up a SIP or IAX client to connect directly to the asterisk server without the need to enter an account and pin each time a call is made. When done, click on the CONFIRM DATA button, then click reload to apply the changes on the Asterisk server.</br>")
-.gettext("The customer must then enter the URL/IP address of the asterisk server into the SIP/IAX client and use the account number and secret as the username and password."),'ListSIPFriend');
+	$CC_help_sipfriend_list = create_help(gettext("Voip Config will create a SIP or IAX entry on the Asterisk server, so that a customer can set up a SIP or IAX client to connect directly to the asterisk server without the need to enter an account and pin each time a call is made. When done, click on the CONFIRM DATA button, then click reload to apply the changes on the Asterisk server.</br>") .
+	gettext("The customer must then enter the URL/IP address of the asterisk server into the SIP/IAX client and use the account number and secret as the username and password."), 'ListSIPFriend');
 
-$CC_help_sipfriend_reload =create_help(gettext("Click reload to commit changes to Asterisk"));
+	$CC_help_sipfriend_reload = create_help(gettext("Click reload to commit changes to Asterisk"));
 
+	$CC_help_generate_signup = create_help(gettext("Generate a specific crypted URL and to configure signup with a customer group and call plan."));
 
-$CC_help_generate_signup=create_help(gettext("Generate a specific crypted URL and to configure signup with a customer group and call plan."));
+	$CC_help_password_change = create_help(gettext("On this page you will be able to change your password, You have to enter the New Password and Confirm it."));
 
-$CC_help_password_change =create_help(gettext("On this page you will be able to change your password, You have to enter the New Password and Confirm it."));
+	$CC_help_secret_change = create_help(gettext("On this page you will be able to change your Secret used to crypt your generated signup URL, You have to enter the New Secret and Confirm it."));
 
-$CC_help_secret_change =create_help(gettext("On this page you will be able to change your Secret used to crypt your generated signup URL, You have to enter the New Secret and Confirm it."));
-	
-$CC_help_list_customer=create_help(gettext("Customers are listed below by card number. Each row corresponds to one customer, along with information such as their call plan, credit remaining, etc.</br>")
-.gettext("The SIP and IAX buttons create SIP and IAX entries to allow direct VoIP connections to the Asterisk server without further authentication."),'ListCustomers');
+	$CC_help_list_customer = create_help(gettext("Customers are listed below by card number. Each row corresponds to one customer, along with information such as their call plan, credit remaining, etc.</br>") .
+	gettext("The SIP and IAX buttons create SIP and IAX entries to allow direct VoIP connections to the Asterisk server without further authentication."), 'ListCustomers');
 
-$CC_help_view_payment =create_help(gettext("Payment history - The section below allows you to add payments against a customer. Note that this does not change the balance on the card. Click refill under customer list to top-up a card."),'ViewPayments');
+	$CC_help_view_payment = create_help(gettext("Payment history - The section below allows you to add payments against a customer. Note that this does not change the balance on the card. Click refill under customer list to top-up a card."), 'ViewPayments');
 
-$CC_help_money_situation =create_help(gettext("This screen shows refills and payments made against each account, along with the current credit on each card. The initial amount of credit applied to the card is not included. The amount owing is calculated by subtracting payments from refills"),'MoneySituation');
+	$CC_help_money_situation = create_help(gettext("This screen shows refills and payments made against each account, along with the current credit on each card. The initial amount of credit applied to the card is not included. The amount owing is calculated by subtracting payments from refills"), 'MoneySituation');
 
-$CC_help_view_refill_agent =create_help(gettext("Agents Refill history - The section below allows you to see your refill"),'ViewAgentRefill');
+	$CC_help_view_refill_agent = create_help(gettext("Agents Refill history - The section below allows you to see your refill"), 'ViewAgentRefill');
 
-$CC_help_view_payment_agent =create_help(gettext("Agent Payment history - The section below allows you to browse your payments"),'ViewPayments');
+	$CC_help_view_payment_agent = create_help(gettext("Agent Payment history - The section below allows you to browse your payments"), 'ViewPayments');
 
-$CC_help_support_list=create_help(gettext("You can see here, all tickets created. You can also add a new ticket for one customer."));
+	$CC_help_support_list = create_help(gettext("You can see here, all tickets created. You can also add a new ticket for one customer."));
 
-$CC_help_signup_agent = create_help(gettext("This shows a list of all signup key create for this agent, this key is used to identify the default paramater for the subscription on the signup page"));
-
+	$CC_help_signup_agent = create_help(gettext("This shows a list of all signup key create for this agent, this key is used to identify the default paramater for the subscription on the signup page"));
 
 } //ENDIF SHOW_HELP
 
+if (!isset ($disable_load_conf) || !($disable_load_conf)) {
 
-if (!isset($disable_load_conf) || !($disable_load_conf)) {
-	
 	$DBHandle = DbConnect();
 	$instance_table = new Table();
 	$QUERY = "SELECT configuration_key FROM cc_configuration where configuration_key in ('MODULE_PAYMENT_AUTHORIZENET_STATUS','MODULE_PAYMENT_PAYPAL_STATUS','MODULE_PAYMENT_MONEYBOOKERS_STATUS','MODULE_PAYMENT_WORLDPAY_STATUS','MODULE_PAYMENT_PLUGNPAY_STATUS') AND configuration_value='True'";
-	$payment_methods  = $instance_table->SQLExec ($DBHandle, $QUERY);
+	$payment_methods = $instance_table->SQLExec($DBHandle, $QUERY);
 	$show_logo = '';
 	for ($index = 0; $index < sizeof($payment_methods); $index++) {
-		if( $payment_methods[$index][0] == "MODULE_PAYMENT_PAYPAL_STATUS") {
-			$show_logo .= '<a href="https://www.paypal.com/en/mrb/pal=PGSJEXAEXKTBU" target="_blank"><img src="'.KICON_PATH.'/paypal_logo.gif" alt="Paypal"/></a> &nbsp; ';
-		//} elseif( $payment_methods[$index][0] == "MODULE_PAYMENT_AUTHORIZENET_STATUS") {
-		//	$show_logo .= '<a href="http://authorize.net/" target="_blank"><img src="'.KICON_PATH.'/authorize.gif" alt="Authorize.net"/></a> &nbsp; ';
-		} elseif( $payment_methods[$index][0] == "MODULE_PAYMENT_MONEYBOOKERS_STATUS") {
-			$show_logo .= '<a href="https://www.moneybookers.com/app/?rid=811621" target="_blank"><img src="'.KICON_PATH.'/moneybookers.gif" alt="Moneybookers"/></a> &nbsp; ';
-		//} elseif( $payment_methods[$index][0] == "MODULE_PAYMENT_WORLDPAY_STATUS") {
-		//	$show_logo .= '<a href="http://www.worldpay.com/" target="_blank"><img src="'.KICON_PATH.'/worldpay.gif" alt="worldpay.com"/></a> &nbsp; ';
-		} elseif( $payment_methods[$index][0] == "MODULE_PAYMENT_PLUGNPAY_STATUS") {
-			$show_logo .= '<a href="http://www.plugnpay.com/" target="_blank"><img src="'.KICON_PATH.'/plugnpay.png" alt="plugnpay.com"/></a> &nbsp; ';
+		if ($payment_methods[$index][0] == "MODULE_PAYMENT_PAYPAL_STATUS") {
+			$show_logo .= '<a href="https://www.paypal.com/en/mrb/pal=PGSJEXAEXKTBU" target="_blank"><img src="' . KICON_PATH . '/paypal_logo.gif" alt="Paypal"/></a> &nbsp; ';
+			//} elseif( $payment_methods[$index][0] == "MODULE_PAYMENT_AUTHORIZENET_STATUS") {
+			//	$show_logo .= '<a href="http://authorize.net/" target="_blank"><img src="'.KICON_PATH.'/authorize.gif" alt="Authorize.net"/></a> &nbsp; ';
+		}
+		elseif ($payment_methods[$index][0] == "MODULE_PAYMENT_MONEYBOOKERS_STATUS") {
+			$show_logo .= '<a href="https://www.moneybookers.com/app/?rid=811621" target="_blank"><img src="' . KICON_PATH . '/moneybookers.gif" alt="Moneybookers"/></a> &nbsp; ';
+			//} elseif( $payment_methods[$index][0] == "MODULE_PAYMENT_WORLDPAY_STATUS") {
+			//	$show_logo .= '<a href="http://www.worldpay.com/" target="_blank"><img src="'.KICON_PATH.'/worldpay.gif" alt="worldpay.com"/></a> &nbsp; ';
+		}
+		elseif ($payment_methods[$index][0] == "MODULE_PAYMENT_PLUGNPAY_STATUS") {
+			$show_logo .= '<a href="http://www.plugnpay.com/" target="_blank"><img src="' . KICON_PATH . '/plugnpay.png" alt="plugnpay.com"/></a> &nbsp; ';
 		}
 	}
-	$PAYMENT_METHOD ='<table style="width:70%;margin:0 auto;" align="center" ><tr><TD valign="top" align="center" class="tableBodyRight">'.$show_logo.'</td></tr></table>';
+	$PAYMENT_METHOD = '<table style="width:70%;margin:0 auto;" align="center" ><tr><TD valign="top" align="center" class="tableBodyRight">' . $show_logo . '</td></tr></table>';
 }
+
 
