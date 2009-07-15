09ca0a2a9ee3cc1ce0c70245b53919c233e40db6
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-07-15 22:52:45 +0000

    add more info on SIP/IAX INFO page
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2196 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- customer/A2B_entity_sipiax_info.php
+++ customer/A2B_entity_sipiax_info.php
@@ -79,7 +79,12 @@ echo $CC_help_sipiax_info;
 			<td colspan="2" class="fontstyle_008">&nbsp;<strong><?php echo $config_name;?></strong> </td>
 		  </tr>
 		  <tr>
-			<td colspan="2" bgcolor="#FFFFFF" class="fontstyle_006" align="center"><br><?php echo gettext("Copy and past this into your ")?> <?php echo $config_file;?><br>
+			<td colspan="2" bgcolor="#FFFFFF" class="fontstyle_006" align="center">
+				<br><b><?php echo $configtype;?> URI :</b> <?php echo SIP_IAX_INFO_HOST; ?><br>
+				<br><b><?php echo gettext("Username")?> :</b> <?php echo $sip_iax_data[0][1]?><br>
+				<br><b><?php echo gettext("Password")?> :</b> <?php echo $sip_iax_data[0][2]?><br><br>
+				
+				<br><?php echo gettext("To configure your Asterisk server, copy and past this into your ")?> <?php echo $config_file;?><br>
 			 
 				<textarea name="textfield" cols="80" rows="12" class="form_input_text" ><?php if(is_array($sip_iax_data)){ ?><?php if($configtype == "IAX"){ ?>[<?php echo SIP_IAX_INFO_TRUNKNAME; ?>]
 username=<?php echo $sip_iax_data[0][1]?>
