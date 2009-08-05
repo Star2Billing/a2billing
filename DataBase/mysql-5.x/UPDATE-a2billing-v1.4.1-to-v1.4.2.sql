de25e06abb7e416c8c8cbca8e4fa188d5a47bbdb
rach <rach@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-08-05 23:47:49 +0000

    Card creation with a new callerid : Correct datatbase
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2286 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/mysql-5.x/UPDATE-a2billing-v1.4.1-to-v1.4.2.sql
+++ DataBase/mysql-5.x/UPDATE-a2billing-v1.4.1-to-v1.4.2.sql
@@ -10,8 +10,10 @@ CREATE TABLE cc_message_agent (
 ) ENGINE = MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-UPDATE cc_config SET config_value = 'PREPAID',
-    config_description = 'billing type of the new card( value : POSTPAID or PREPAID) .',
-    config_listvalues = 'PREPAID,POSTPAID' WHERE config_key ='cid_auto_create_card_typepaid';
-UPDATE config SET config_value = '1' WHERE config_key ='cid_auto_create_card_credit_limit';
-UPDATE config SET config_value = '1' WHERE config_key ='cid_auto_create_card_tariffgroup';
+INSERT INTO `cc_config` ( `config_title`, `config_key`, `config_value`, `config_description`, `config_valuetype`, `config_listvalues`, `config_group_title`) VALUES( 'Auto Create Card', 'cid_auto_create_card', '0', 'if the callerID is captured on a2billing, this option will create automatically a new card and add the callerID to it.', 1, 'yes,no', 'agi-conf1');
+INSERT INTO `cc_config` ( `config_title`, `config_key`, `config_value`, `config_description`, `config_valuetype`, `config_listvalues`, `config_group_title`) VALUES( 'Auto Create Card Length', 'cid_auto_create_card_len', '10', 'set the length of the card that will be auto create (ie, 10).', 0, NULL, 'agi-conf1');
+INSERT INTO `cc_config` ( `config_title`, `config_key`, `config_value`, `config_description`, `config_valuetype`, `config_listvalues`, `config_group_title`) VALUES( 'Auto Create Card Type', 'cid_auto_create_card_typepaid', 'PREPAID', 'billing type of the new card( value : POSTPAID or PREPAID) .', 0, NULL, 'agi-conf1');
+INSERT INTO `cc_config` ( `config_title`, `config_key`, `config_value`, `config_description`, `config_valuetype`, `config_listvalues`, `config_group_title`) VALUES( 'Auto Create Card Credit', 'cid_auto_create_card_credit', '0', 'amount of credit of the new card.', 0, NULL, 'agi-conf1');
+INSERT INTO `cc_config` ( `config_title`, `config_key`, `config_value`, `config_description`, `config_valuetype`, `config_listvalues`, `config_group_title`) VALUES( 'Auto Create Card Limit', 'cid_auto_create_card_credit_limit', '0', 'if postpay, define the credit limit for the card.', 0, NULL, 'agi-conf1');
+INSERT INTO `cc_config` ( `config_title`, `config_key`, `config_value`, `config_description`, `config_valuetype`, `config_listvalues`, `config_group_title`) VALUES( 'Auto Create Card TariffGroup', 'cid_auto_create_card_tariffgroup', '1', 'the tariffgroup to use for the new card (this is the ID that you can find on the admin web interface) .', 0, NULL, 'agi-conf1');
+
