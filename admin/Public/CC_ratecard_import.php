b59ed37d928684cbcebc932e7ca9a63a5bf1e1fc
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-08-05 00:01:47 +0000

    Ticket #611 : Import rates issue : French language
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2279 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- admin/Public/CC_ratecard_import.php
+++ admin/Public/CC_ratecard_import.php
@@ -36,12 +36,12 @@ $smarty->display('main.tpl');
 
 function sendtoupload(form){
 	if (form.tariffplan.value.length < 1){
-		alert ('<?php echo gettext("Please, you must first select a ratecard !")?>');
+		alert ('<?php echo addslashes(gettext("Please, you must first select a ratecard !")); ?>');
 		form.tariffplan.focus ();
 		return (false);
 	}
 	if (form.the_file.value.length < 2){
-		alert ('<?php echo gettext("Please, you must first select a file !")?>');
+		alert ('<?php echo addslashes(gettext("Please, you must first select a file !")); ?>');
 		form.the_file.focus ();
 		return (false);
 	}
