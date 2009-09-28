0c5382299bf8085cb66e6bee8244f23f36e5a76a
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-09-28 14:29:45 +0000

    rebuild PO & MO
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2400 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- common/lib/common.defines.php
+++ common/lib/common.defines.php
@@ -118,7 +118,7 @@ define ("TEXTCONTACT", gettext("This software has been created by Areski under A
 define ("EMAILCONTACT", "areski@gmail.com");
 
   
-define ("COPYRIGHT", "A2Billing - Version 1.4.2 (Radix) - Released : 30 July, is software licensed under the ".'<a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html" target="_blank">AGPL 3</a>'.". <br/>" .
+define ("COPYRIGHT", "A2Billing - Version 1.4.2 (Radix) - Released : 28 September 2009, A2Billing is software licensed under the ".'<a href="http://www.fsf.org/licensing/licenses/agpl-3.0.html" target="_blank">AGPL 3</a>'.". <br/>" .
 		"Copyright (C) 2004-2009 - Star2billing S.L. <a href=\"http://www.star2billing.com\" target=\"_blank\">http://www.star2billing.com/</a>");
 
 define ("CCMAINTITLE", gettext("A2Billing Portal"));
