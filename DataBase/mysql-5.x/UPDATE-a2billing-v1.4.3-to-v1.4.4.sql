99dc886f1d2c56ec68223ce518b4fd1b8f45f0eb
areski <areski@9d51a8f0-6521-42ea-932e-d822aa02ce2f>
2009-11-30 17:30:22 +0000

    clean sql script 1.4.4
    git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2548 9d51a8f0-6521-42ea-932e-d822aa02ce2f

--- DataBase/mysql-5.x/UPDATE-a2billing-v1.4.3-to-v1.4.4.sql
+++ DataBase/mysql-5.x/UPDATE-a2billing-v1.4.3-to-v1.4.4.sql
@@ -33,45 +33,33 @@
 
 
 CREATE VIEW cc_callplan_lcr AS
-SELECT cc_ratecard.destination, cc_ratecard.dialprefix, cc_ratecard.buyrate, cc_ratecard.rateinitial, cc_ratecard.startdate, cc_ratecard.stopdate, cc_ratecard.initblock, cc_ratecard.connectcharge, cc_ratecard.id_trunk , cc_ratecard.idtariffplan , cc_ratecard.id, cc_tariffgroup.id AS tariffgroup_id
- 
-FROM cc_tariffgroup 
-RIGHT JOIN cc_tariffgroup_plan ON cc_tariffgroup_plan.idtariffgroup=cc_tariffgroup.id 
-INNER JOIN cc_tariffplan ON (cc_tariffplan.id=cc_tariffgroup_plan.idtariffplan ) 
-LEFT JOIN cc_ratecard ON cc_ratecard.idtariffplan=cc_tariffplan.id;
+	SELECT cc_ratecard.destination, cc_ratecard.dialprefix, cc_ratecard.buyrate, cc_ratecard.rateinitial, cc_ratecard.startdate, cc_ratecard.stopdate, cc_ratecard.initblock, cc_ratecard.connectcharge, cc_ratecard.id_trunk , cc_ratecard.idtariffplan , cc_ratecard.id, cc_tariffgroup.id AS tariffgroup_id
+	FROM cc_tariffgroup 
+	RIGHT JOIN cc_tariffgroup_plan ON cc_tariffgroup_plan.idtariffgroup=cc_tariffgroup.id 
+	INNER JOIN cc_tariffplan ON (cc_tariffplan.id=cc_tariffgroup_plan.idtariffplan ) 
+	LEFT JOIN cc_ratecard ON cc_ratecard.idtariffplan=cc_tariffplan.id;
 
 
 -- New Agent commission module
-ALTER TABLE `cc_agent` ADD `com_balance` DECIMAL( 15, 5 ) NOT NULL;
-ALTER TABLE `cc_agent_commission` DROP `paid_status` ;
-ALTER TABLE `cc_agent_commission` ADD `commission_type` TINYINT NOT NULL ;
-ALTER TABLE `cc_agent_commission` ADD `commission_percent` DECIMAL( 10, 4 ) NOT NULL ;
-INSERT INTO `a2billing`.`cc_config` (
-	`id` ,
-	`config_title` ,
-	`config_key` ,
-	`config_value` ,
-	`config_description` ,
-	`config_valuetype` ,
-	`config_listvalues` ,
-	`config_group_title`
-) VALUES (	NULL , 'Authorize Remittance Request', 'remittance_request', '1', 'Enable or disable the link which allow agent to submit a remittance request', '0', 'yes,no', 'webagentui');
+ALTER TABLE cc_agent ADD com_balance DECIMAL( 15, 5 ) NOT NULL;
+ALTER TABLE cc_agent_commission DROP paid_status ;
+ALTER TABLE cc_agent_commission ADD commission_type TINYINT NOT NULL ;
+ALTER TABLE cc_agent_commission ADD commission_percent DECIMAL( 10, 4 ) NOT NULL ;
+INSERT INTO cc_config ( config_title , config_key , config_value , config_description , config_valuetype , config_listvalues , config_group_title) 
+VALUES ('Authorize Remittance Request', 'remittance_request', '1', 'Enable or disable the link which allow agent to submit a remittance request', '0', 'yes,no', 'webagentui');
 
-ALTER TABLE `cc_agent` ADD `threshold_remittance` DECIMAL( 15, 5 ) NOT NULL ;
-ALTER TABLE `cc_agent` ADD `bank_info` MEDIUMTEXT NULL ;
 
-CREATE TABLE `a2billing`.`cc_remittance_request` (
-`id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`id_agent` BIGINT NOT NULL ,
-`amount` DECIMAL( 15, 5 ) NOT NULL ,
-`type` TINYINT NOT NULL
-) ENGINE = MYISAM ;
+ALTER TABLE cc_agent ADD threshold_remittance DECIMAL( 15, 5 ) NOT NULL ;
+ALTER TABLE cc_agent ADD bank_info MEDIUMTEXT NULL ;
 
-ALTER TABLE `cc_remittance_request` ADD `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-ADD `status` TINYINT NOT NULL ,
-ADD `description` MEDIUMTEXT NULL;
+CREATE TABLE cc_remittance_request (
+	id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+	id_agent BIGINT NOT NULL ,
+	amount DECIMAL( 15, 5 ) NOT NULL ,
+	type TINYINT NOT NULL,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+	status TINYINT NOT NULL DEFAULT '0',
+	description MEDIUMTEXT NULL
+) ENGINE = MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-ALTER TABLE `cc_remittance_request` ADD `status` TINYINT NOT NULL DEFAULT '0',
-ADD `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-ADD `description` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_bin NULL;
 
