4bdfefefe97748548019f5c1a523b7f9d7989c75
Areski Belaid <areski@gmail.com>
2010-10-25 17:15:36 +0200

    remove template generated file from Smarty

--- admin/templates_c/%%D6^D6F^D6F3F739%%profiler.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.25-dev, created on 2010-10-25 13:05:25
-         compiled from profiler.tpl */ ?>
-<?php  
-        global $profiler;
-        global $G_instance_Query_trace;
-
-        if ($profiler->installed && $profiler->modedebug)
-                $profiler->display($G_instance_Query_trace);
- ?>
