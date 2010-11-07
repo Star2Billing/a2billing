f4a39405c0f809d721673135b5d124eb66462847
Areski Belaid <areski@gmail.com>
2010-11-07 13:10:25 +0100

    path fix on build-merge-all-po-mo-gettext.sh script

--- addons/contrib/gettext/build-merge-all-po-mo-gettext.sh
+++ addons/contrib/gettext/build-merge-all-po-mo-gettext.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 #
-# AGPL (c) 2009 areski@gmail.com
-# Asterisk2Billing : Autogenerate gettext & merge
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 #
+#  Asterisk2Billing : Autogenerate gettext & merge
+#
+#
+#  This file is part of A2Billing (http://www.a2billing.net/)
+# 
+#  A2Billing, Commercial Open Source Telecom Billing platform,   
+#  powered by Star2billing S.L. <http://www.star2billing.com/>
+#  
+#  @copyright   Copyright (C) 2004-2009 - Star2billing S.L. 
+#  @author      Belaid Arezqui <areski@gmail.com>
+#  @license     http://www.fsf.org/licensing/licenses/agpl-3.0.html
+#  @package     A2Billing
+# 
+#  Software License Agreement (GNU Affero General Public License)
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+# 
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  
+#  
 
-# Get the path of this script
+
+# got to the a2billing directory
+cd ../../../
+
+# path to run the script
 cwd=`pwd`
 
 PO_DIRECTORY_CUSTOMER='common/cust_ui_locale'
@@ -15,8 +47,7 @@ DIR_CUSTOMER='customer'
 DIR_ADMIN='admin'
 DIR_AGENT='agent'
 
-# got to the a2billing directory
-cd ../../../
+
 
 show_menu(){
 	echo "which PO do you want to generate ? "
