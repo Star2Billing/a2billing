Star2Billing/a2billing

commit d32741826c40d08da77583d01150880124a84eb5

Fixes for PHP 5.3 support
git-svn-id: http://svn.a2billing.net/svn/asterisk2billing/trunk@2861 9d51a8f0-6521-42ea-932e-d822aa02ce2f

common/lib/Class.MytoPg.php
@@ -57,7 +57,7 @@ class MytoPg {
 			$pos = 0;
 			while ($pos < strlen($q)) {
 				$slices++;
-				$f = eregi($this->mytopg[$i], substr($q, $pos), $match);
+				$f = preg_match("/".$this->mytopg[$i]."/i", substr($q, $pos), $match);
 
 				// there are no matches; use the remainder of the input verbatim
 				if ($f === FALSE) {
@@ -104,9 +104,9 @@ class MytoPg {
 						$new = $match[1].'date_part(\'epoch\','.$match[3];
 						$pos = $matchpos + strlen($match[1].$match[3]) + 18;
 
-					} elseif (eregi('LIMIT([[:space:]])([[:digit:]]*)'
+					} elseif (preg_match('/LIMIT([[:space:]])([[:digit:]]*)'
 						.'([[:space:]]*),([[:space:]]*)'
-						.'([[:digit:]]*)', $match[2], $params)) {
+						.'([[:digit:]]*)/i', $match[2], $params)) {
 						// slightly more complex; we've got two parameters too
 						$new = "$match[1]LIMIT$params[1]$params[5]$params[3] "
 							."OFFSET $params[4]$params[2]$match[3]";
@@ -137,8 +137,8 @@ class MytoPg {
 						// first we need to add a little escaping
 						foreach ($rep as &$value) {
 							// if a string literal contains '.',  add escaping
-							if (ereg('^\'.*[.].*\'$', trim(ltrim($value)))) {
-								$value = ' E'.ereg_replace('([^\\])[.]', '\1\\\\.', trim(ltrim($value)));
+							if (preg_match('/^\'.*[.].*\'$/', trim(ltrim($value)))) {
+								$value = ' E'.preg_replace('/([^\\])[.]/', '\1\\\\.', trim(ltrim($value)));
 							}
 						}
 						unset($value);
@@ -146,7 +146,7 @@ class MytoPg {
 							.implode($rep, ',').", 'g')";
 						$pos = $matchpos + strlen($match[1].$match[3])+9;
 					
-					} elseif (ereg('(ADD|SUB)DATE|DATE_(ADD|SUB)', $matched, $tmp)) {
+					} elseif (preg_match('/(ADD|SUB)DATE|DATE_(ADD|SUB)/', $matched, $tmp)) {
 						// determine whether to add or subtract
 						if ($tmp[1] == 'SUB' || $tmp[2] == 'SUB') {
 							$sign = ' - ';
@@ -170,7 +170,7 @@ class MytoPg {
 					} elseif ('SUBSTRING' == $matched) {
 						if ($dbg > 3) $d.=">>>> $match[2] : \$exp>$exp< \$rep[0-2]>$rep[0]<>$rep[1]<>$rep[2]<";
 						// if it looks like a field name containing time or date
-						if (eregi('\s*(\w*time|date\w*)\s*', $rep[0])) {
+						if (preg_match('/\s*(\w*time|date\w*)\s/i*', $rep[0])) {
 							if ($rep[1] == 1 && $rep[2] == 10) {
 								// rewrite as cast to datestamp
 								$new = "$match[1]$match[3]("
@@ -211,10 +211,10 @@ class MytoPg {
 
 					} elseif ('DATETIME' == $matched) {
 						// if it looks like a field name containing time or date
-						if (eregi('(\s*\w*)(time|date)(\w*)(\s*)', $rep[0], $parms)) {
+						if (preg_match('/(\s*\w*)(time|date)(\w*)(\s*)/i', $rep[0], $parms)) {
 							// add a cast to timestamp
 							$rep[0] = "$parm[1]$parm[2]$parm[3]::timestamp$parm[4]";
-						} elseif (eregi('\'now\'', $rep[0], $parms)) {
+						} elseif (preg_match('/\'now\'/', $rep[0], $parms)) {
 							$rep[0] = "now()";
 						} else {
 							$rep[0] = $this -> Cast_date_part($rep[0]);
@@ -391,13 +391,13 @@ class MytoPg {
 	// If a date participle looks like an immediate constant, cast it appropriately
 	function Cast_date_part ($part) {
 
-		if (eregi('([[:space:]]*)[\'"]now[\'"]([[:space:]]*)', $part, $parm)) {
+		if (preg_match('/([[:space:]]*)[\'"]now[\'"]([[:space:]]*)/i', $part, $parm)) {
 			$part = "$parm[1]now()$parm[2]";
 //                                                 |      year         | month 0?1-1     | day 0?1-31           |      hours 0?0-24     | min/secs 0?0-59     |  Timezone? [-+]0-1459] or ABC        |
-		} elseif (ereg ('([[:space:]]*)([\'"][[:space:]]*[[:digit:]]{4}-[0-1]?[[:digit:]]-[0-3]?[[:digit:]][[:space:]][0-2]?[[:digit:]](:[0-5]?[[:digit:]]){2}([[:space:]]?([-+][[:digit:]]{1,4}|[[:alpha:]]{2,3}))?[[:space:]]*[\'"])([[:space:]]*)', $part, $parm)) {
+		} elseif (preg_match ('/([[:space:]]*)([\'"][[:space:]]*[[:digit:]]{4}-[0-1]?[[:digit:]]-[0-3]?[[:digit:]][[:space:]][0-2]?[[:digit:]](:[0-5]?[[:digit:]]){2}([[:space:]]?([-+][[:digit:]]{1,4}|[[:alpha:]]{2,3}))?[[:space:]]*[\'"])([[:space:]]*)/', $part, $parm)) {
 			$part = "$parm[1]$parm[2]::timestamp$parm[6]";
 //                                                 |      year         | month 0?1-12    | day 0?1-31           |
-		} elseif (ereg ('([[:space:]]*)([\'"][[:space:]]*[[:digit:]]{4}-[0-1]?[[:digit:]]-[0-3]?[[:digit:]][[:space:]]*[\'"])([[:space:]]*)', $part, $parm)) {
+		} elseif (preg_match ('/([[:space:]]*)([\'"][[:space:]]*[[:digit:]]{4}-[0-1]?[[:digit:]]-[0-3]?[[:digit:]][[:space:]]*[\'"])([[:space:]]*)/', $part, $parm)) {
 			$part = "$parm[1]$parm[2]::date$parm[3]";
 		}
 		return $part;

admin/Public/A2B_detail_package.php
@@ -457,7 +457,7 @@ $smarty->display('main.tpl');
 									$select_list = $instance_sub_table -> Get_list ($DBHandle, $sub_clause, null, null, null, null, null, null);
 									
 									
-									$field_list_sun = split(',',$FG_TABLE_COL[$i][8]);
+									$field_list_sun = preg_split('/,/',$FG_TABLE_COL[$i][8]);
 									$record_display = $FG_TABLE_COL[$i][10];
 									
 									for ($l=1;$l<=count($field_list_sun);$l++){										

admin/Public/A2B_entity_def_ratecard.php
@@ -203,7 +203,7 @@ if ($form_action != "list") {
 }
 
 if (is_string($tariffgroup) && strlen(trim($tariffgroup)) > 0) {
-	list ($mytariffgroup_id, $mytariffgroupname, $mytariffgrouplcrtype) = split('-:-', $tariffgroup);
+	list ($mytariffgroup_id, $mytariffgroupname, $mytariffgrouplcrtype) = preg_split('/-:-/', $tariffgroup);
 	$_SESSION["mytariffgroup_id"] = $mytariffgroup_id;
 	$_SESSION["mytariffgroupname"] = $mytariffgroupname;
 	$_SESSION["tariffgrouplcrtype"] = $mytariffgrouplcrtype;

admin/Public/A2B_entity_did_import_analyse.php
@@ -50,12 +50,12 @@ check_demo_mode();
 getpost_ifset(array('didgroup', 'search_sources', 'task', 'status','countryID','uploadedfile_name'));
 
 
-$didgroupval= split('-:-', $didgroup);
+$didgroupval= preg_split('/-:-/', $didgroup);
 if (!is_numeric($didgroupval[0])){
 	echo gettext("No DIDGroup defined !");
 	exit();
 }
-$countryIDval= split('-:-', $countryID);
+$countryIDval= preg_split('/-:-/', $countryID);
 if (!is_numeric($countryIDval[0])){
 	echo gettext("No Country defined !");
 	exit();
@@ -64,7 +64,7 @@ if (!is_numeric($countryIDval[0])){
 if ($search_sources!='nochange'){
 
 	//echo "<br>---$search_sources";
-	$fieldtoimport= split("\t", $search_sources);
+	$fieldtoimport= preg_split("/\t/", $search_sources);
 	$fieldtoimport_sql = str_replace("\t", ", ", $search_sources);
 	$fieldtoimport_sql = trim ($fieldtoimport_sql);
 	if (strlen($fieldtoimport_sql)>0) $fieldtoimport_sql = ', '.$fieldtoimport_sql;
@@ -162,7 +162,7 @@ if ($task=='upload'){
 		for ($i = 0; $i < strlen($chaine1); $i++)
 			$ligne = str_replace($chaine1[$i], ' ', $ligneoriginal);
 		
-		$val = split('[;,]', $ligne);
+		$val = preg_split('/[;,]/', $ligne);
 		$val[0]=str_replace('"', '', $val[0]); //DH
 		$val[1]=str_replace('"', '', $val[1]); //DH
 		$val[2]=str_replace('"', '', $val[2]); //DH

admin/Public/A2B_phonelist_import_analyse.php
@@ -49,7 +49,7 @@ check_demo_mode();
 getpost_ifset(array('phonebook', 'search_sources', 'task','status_import','uploadedfile_name','uploadedfile_name'));
 
 
-$phonebookval= split('-:-', $phonebook);
+$phonebookval= preg_split('/-:-/', $phonebook);
 if (!is_numeric($phonebookval[0])){ 
 	echo gettext("No Phonebook defined !"); 
 	exit();
@@ -57,7 +57,7 @@ if (!is_numeric($phonebookval[0])){
 
 
 if ($search_sources!='nochange'){
-	$fieldtoimport= split("\t", $search_sources);
+	$fieldtoimport= preg_split("/\t/", $search_sources);
 	$fieldtoimport_sql = str_replace("\t", ", ", $search_sources);
 	$fieldtoimport_sql = trim ($fieldtoimport_sql);
 	if (strlen($fieldtoimport_sql)>0) $fieldtoimport_sql = ', '.$fieldtoimport_sql;
@@ -135,7 +135,7 @@ if ($task=='upload') {
 		// strip out ' and " and, with the exception of dialprefix field,
 		// substitute , for . to allow European style floats, eg: 0,1 == 0.1
 		$ligne = str_replace(array('"',"'"), '', $ligneoriginal);
-		$val=split('[;:]', $ligne);
+		$val=preg_split('/[;:]/', $ligne);
 		for ($i = 1; $i < count($val); $i++)
 			 $val[$i]=str_replace(',', '.', $val[$i]);
 		if ($status_import!="ok") break;

admin/Public/CC_card_import_analyse.php
@@ -50,7 +50,7 @@ check_demo_mode();
 getpost_ifset(array('search_sources', 'task', 'status','uploadedfile_name'));
 
 if ($search_sources!='nochange') {
-	$fieldtoimport= split("\t", $search_sources);
+	$fieldtoimport= preg_split("/\t/", $search_sources);
 	$fieldtoimport_sql = str_replace("\t", ", ", $search_sources);
 	$fieldtoimport_sql = trim ($fieldtoimport_sql);
 	if (strlen($fieldtoimport_sql)>0) $fieldtoimport_sql = ', '.$fieldtoimport_sql;
@@ -146,7 +146,7 @@ if ($task=='upload') {
 		for ($i = 0; $i < strlen($chaine1); $i++)
 			$ligne = str_replace($chaine1[$i], ' ', $ligneoriginal);
 		
-		$val = split('[;,]', $ligne);
+		$val = preg_split('/[;,]/', $ligne);
 		$val[0]=str_replace('"', '', $val[0]); //DH
 		$val[1]=str_replace('"', '', $val[1]); //DH
 		$val[2]=str_replace('"', '', $val[2]); //DH

admin/Public/CC_entity_alarm_details.php
@@ -196,7 +196,7 @@ function openURL(theLINK)
 									$select_list = $instance_sub_table -> Get_list ($DBHandle, $sub_clause, null, null, null, null, null, null);
 									
 									
-									$field_list_sun = split(',',$FG_TABLE_COL[$i][8]);
+									$field_list_sun = preg_split('/,/',$FG_TABLE_COL[$i][8]);
 									$record_display = $FG_TABLE_COL[$i][10];
 									
 									for ($l=1;$l<=count($field_list_sun);$l++){										

admin/Public/CC_entity_service_details.php
@@ -175,7 +175,7 @@ $smarty->display('main.tpl');
 									$select_list = $instance_sub_table -> Get_list ($DBHandle, $sub_clause, null, null, null, null, null, null);
 									
 									
-									$field_list_sun = split(',',$FG_TABLE_COL[$i][8]);
+									$field_list_sun = preg_split('/,/',$FG_TABLE_COL[$i][8]);
 									$record_display = $FG_TABLE_COL[$i][10];
 									
 									for ($l=1;$l<=count($field_list_sun);$l++){										

commit 555779f74c5dc77aa08f2cc3cccab7ab58372e2f

Fix breakage in MytoPg introduced by r2861 which changed ereg() to preg_match() to fix deprecation warnings from PHP 5.3. As a bonus this virtually halves the CPU time taken; PCRE FTW! Also fixes various other PHP 5.3 strict warnings and notices.

common/lib/Class.MytoPg.php
@@ -45,6 +45,7 @@ class MytoPg {
 		$count = 0;
 		$slices = 0;
 		$d = '';
+		$matchpos = 0;
 
 		$start = microtime(true);
 		$old_length = strlen($q);
@@ -60,7 +61,7 @@ class MytoPg {
 				$f = preg_match("/".$this->mytopg[$i]."/i", substr($q, $pos), $match);
 
 				// there are no matches; use the remainder of the input verbatim
-				if ($f === FALSE) {
+				if (!$f) {
 					if ($dbg > 3) $d .= ">>>> Skipping from pos $pos";
 					$pos = strlen($q);
 					continue;
@@ -75,7 +76,7 @@ class MytoPg {
 				}
 
  				// we have a match;  process it and form the replacement text
- 				$matchpos = strpos($q, $match[2], $pos);
+ 				$matchpos = strpos($q, $match[0], $pos);
  				$matched = strtoupper($match[2]);
 				if ($dbg > 3) $d .= ">>>> Matched $match[2] at $matchpos";
 
@@ -88,7 +89,7 @@ class MytoPg {
 						// this is just a simple replacement,  nothing difficult
 						$new = $match[1].'~*'.$match[3];
 						$pos = $matchpos + strlen($match[1].$match[3]) + 2;
-						
+
 					} elseif ('TRUNCATE' == $matched) {
 						// this is just a simple replacement,  nothing difficult
 						$new = $match[1].'TRUNC'.$match[3];
@@ -122,8 +123,8 @@ class MytoPg {
 					// These are more difficult as we must find the matching
 					// closing bracket, and thus the whole braced expression
 					$end = $this -> Parse_helper('brace', $q, $matchpos, $dbg, $d);
-					$exp = substr($q, $matchpos+strlen($match[2].$match[3])+1, $end-$matchpos-strlen($match[2].$match[3])-1);
-					$remove = strlen($match[2].$match[3].$exp) + 2;
+					$exp = substr($q, $matchpos+strlen($match[0]), $end-$matchpos-strlen($match[0]));
+					$remove = strlen($match[0].$exp) + 1;
 
 					// split each element of this braced expression on comma
 					$rep = $this -> Parse_helper('split', $exp, ',', $dbg, $d);
@@ -138,24 +139,25 @@ class MytoPg {
 						foreach ($rep as &$value) {
 							// if a string literal contains '.',  add escaping
 							if (preg_match('/^\'.*[.].*\'$/', trim(ltrim($value)))) {
-								$value = ' E'.preg_replace('/([^\\])[.]/', '\1\\\\.', trim(ltrim($value)));
+								$value = preg_replace('/\'(.*)([.])(.*)\'/', ' E\'$1\\\\\\\$2$3\'', trim(ltrim($value)));
 							}
 						}
 						unset($value);
 						$new = "$match[1]REGEXP_REPLACE$match[3]("
 							.implode($rep, ',').", 'g')";
 						$pos = $matchpos + strlen($match[1].$match[3])+9;
-					
+
 					} elseif (preg_match('/(ADD|SUB)DATE|DATE_(ADD|SUB)/', $matched, $tmp)) {
 						// determine whether to add or subtract
-						if ($tmp[1] == 'SUB' || $tmp[2] == 'SUB') {
+						if ($tmp[1] == 'SUB' || (sizeof($tmp) > 2 && $tmp[2] == 'SUB')) {
 							$sign = ' - ';
 						} else {
 							$sign = ' + ';
 						}
 
 						// MySQL's ADD/SUBDATE has two possible syntaxes
-						$tmp = $this -> Parse_helper('split', ltrim($rep[1]), ' ', $dbg, $d);
+						$tmp = ltrim($rep[1]);
+						$tmp = $this -> Parse_helper('split', $tmp, ' ', $dbg, $d);
 						if ($dbg > 3) $d.=">>>> Found $match[2]$match[3]\$rep:>$rep[1]<\t>$rep[2]<\t>$rep[3]< , $tmp[0] '$tmp[1] $tmp[2]'";
 						if (sizeof($tmp) == 1) {
 							$rep[1] = "INTERVAL '$tmp[0] DAYS'";
@@ -170,7 +172,7 @@ class MytoPg {
 					} elseif ('SUBSTRING' == $matched) {
 						if ($dbg > 3) $d.=">>>> $match[2] : \$exp>$exp< \$rep[0-2]>$rep[0]<>$rep[1]<>$rep[2]<";
 						// if it looks like a field name containing time or date
-						if (preg_match('/\s*(\w*time|date\w*)\s/i*', $rep[0])) {
+						if (preg_match('/\s*(\w*time|date\w*)\s*/i', $rep[0])) {
 							if ($rep[1] == 1 && $rep[2] == 10) {
 								// rewrite as cast to datestamp
 								$new = "$match[1]$match[3]("

addons/contrib/test-cases/test-0001-MytoPg.php
@@ -328,18 +328,18 @@ $t[$i++] = array("MytoPg(): ADDDATE(1,3) from call-daily-load.php date clause",
 
 
 $t[$i++] = array("MytoPg(): AGI/a2billing.php callback spool",
-"INSERT INTO cc_callback_spool (uniqueid, status, server_ip, num_attempt, channel, exten, context, priority, variable, id_server_group, callback_time, account, callerid, timeout ) VALUES ('$uniqueid', '$status', '$server_ip', '$num_attempt', '$channel', '$exten', '$context', '$priority', '$variable', '$id_server_group', ADDDATE( CURRENT_TIMESTAMP, INTERVAL 1 SECOND ), '$account', '$callerid', '$timeout')",
-"INSERT INTO cc_callback_spool (uniqueid, status, server_ip, num_attempt, channel, exten, context, priority, variable, id_server_group, callback_time, account, callerid, timeout ) VALUES ('$uniqueid', '$status', '$server_ip', '$num_attempt', '$channel', '$exten', '$context', '$priority', '$variable', '$id_server_group', ( CURRENT_TIMESTAMP + INTERVAL '1 SECOND'), '$account', '$callerid', '$timeout')");
+"INSERT INTO cc_callback_spool (uniqueid, status, server_ip, num_attempt, channel, exten, context, priority, variable, id_server_group, callback_time, account, callerid, timeout ) VALUES ('', '', '', '', '', '', '', '', '', '', ADDDATE( CURRENT_TIMESTAMP, INTERVAL 1 SECOND ), '', '', '')",
+"INSERT INTO cc_callback_spool (uniqueid, status, server_ip, num_attempt, channel, exten, context, priority, variable, id_server_group, callback_time, account, callerid, timeout ) VALUES ('', '', '', '', '', '', '', '', '', '', ( CURRENT_TIMESTAMP + INTERVAL '1 SECOND'), '', '', '')");
 
 
 $t[$i++] = array("MytoPg(): admin/api/SOAP/callback-exec.php time clause",
-"SELECT '$uniqueid', '$status', '$server_ip', '$num_attempt', '$channel', '$exten', '$context', '$priority', '$variable', '$id_server_group', ADDDATE( CURRENT_TIMESTAMP, INTERVAL 1 SECOND ), '$account', '$callerid', '30000'",
-"SELECT '$uniqueid', '$status', '$server_ip', '$num_attempt', '$channel', '$exten', '$context', '$priority', '$variable', '$id_server_group', ( CURRENT_TIMESTAMP + INTERVAL '1 SECOND'), '$account', '$callerid', '30000'");
+"SELECT '', '', '', '', '', '', '', '', '', '', ADDDATE( CURRENT_TIMESTAMP, INTERVAL 1 SECOND ), '', '', '30000'",
+"SELECT '', '', '', '', '', '', '', '', '', '', ( CURRENT_TIMESTAMP + INTERVAL '1 SECOND'), '', '', '30000'");
 
 
 $t[$i++] = array("MytoPg(): admin/Public/graph_hourdetail.php date clause",
-" AND t1.starttime < '$fromstatsmonth_sday-$fromstatsday_sday ".$hourintervalplus.":00:00' AND t1.starttime >= '$fromstatsmonth_sday-$fromstatsday_sday ".$hourinterval.":00:00' ",
-" AND t1.starttime < '$fromstatsmonth_sday-$fromstatsday_sday ".$hourintervalplus.":00:00' AND t1.starttime >= '$fromstatsmonth_sday-$fromstatsday_sday ".$hourinterval.":00:00' ");
+" AND t1.starttime < '12-12 12:00:00' AND t1.starttime >= '12-12 00:00:00' ",
+" AND t1.starttime < '12-12 12:00:00' AND t1.starttime >= '12-12 00:00:00' ");
 
 
 
@@ -416,6 +416,9 @@ $t[$i++] = array("MytoPg(): Cast_date_part's timezone support and non-zero-padde
 //  (or for syslog:)  tail -F /var/log/messages | sed -e 's/>>>>/\n/g'
 $dbg = 0;		// 0: CPU hogs,  1: overview,  2: detail 3:more 4:all
 $times = 1;	// average time this many runs, >=30 for less jitter when optimising
+$fail = 0;	// count the number of test failures
+$pass = 0;	// count the number of test passes
+$res = '';	// a temporary holder for SQL results
 $total_time = 0;
 for ($i = 0;  $i < sizeof($t);  $i++) {
 	if ($dbg) print "\n";