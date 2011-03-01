267b989acecee7737859dc0485ef1afefdfbfa12
Areski Belaid <areski@gmail.com>
2011-03-01 17:48:17 +0100

    prepare release v1.9.2

--- AGI/a2billing.php
+++ AGI/a2billing.php
@@ -49,7 +49,7 @@ include (dirname(__FILE__)."/lib/interface/constants.php");
 
 $charge_callback = 0;
 $G_startime = time();
-$agi_version = "A2Billing - Version 1.9.1 (Cuprum)";
+$agi_version = "A2Billing - Version 1.9.2 (Cuprum)";
 
 if ($argc > 1 && ($argv[1] == '--version' || $argv[1] == '-v')) {
 	echo "$agi_version\n";
