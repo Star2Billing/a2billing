bc65a9e489d438e27d139989d0d63f1dda140f62
Areski Belaid <areski@gmail.com>
2012-09-25 15:30:35 +0200

    update mysql migration files

--- DataBase/mysql-5.x/UPDATE-a2billing-v1.6.2-to-v1.7.0.sql
+++ DataBase/mysql-5.x/UPDATE-a2billing-v1.6.2-to-v1.7.0.sql
@@ -4,10 +4,10 @@
 /**
  * This file is part of A2Billing (http://www.a2billing.net/)
  *
- * A2Billing, Commercial Open Source Telecom Billing platform,   
+ * A2Billing, Commercial Open Source Telecom Billing platform,
  * powered by Star2billing S.L. <http://www.star2billing.com/>
- * 
- * @copyright   Copyright (C) 2004-2012 - Star2billing S.L. 
+ *
+ * @copyright   Copyright (C) 2004-2012 - Star2billing S.L.
  * @author      Belaid Arezqui <areski@gmail.com>
  * @license     http://www.fsf.org/licensing/licenses/agpl-3.0.html
  * @package     A2Billing
@@ -26,8 +26,8 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * 
+ *
+ *
 **/
 
 
@@ -96,8 +96,5 @@ ALTER TABLE cc_card_group CHANGE description description VARCHAR( 400 ) CHARACTE
 ALTER TABLE cc_config CHANGE config_description config_description VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL;
 
 
-  
+-- Update Version
 UPDATE cc_version SET version = '1.7.0';
-
-
-
