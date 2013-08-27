6b907116bc28c1a11520ff39e34a3e7802b61250
Areski Belaid <areski@gmail.com>
2013-08-27 17:18:49 +0200

    update version v2.0.6

--- DataBase/mysql-5.x/UPDATE-a2billing-v2.0.4-to-v2.0.5.sql
+++ DataBase/mysql-5.x/UPDATE-a2billing-v2.0.4-to-v2.0.5.sql
@@ -27,8 +27,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Contributed by Milan Benicek, SIPhone s.r.o., milan.benicek@siphone.cz
- *
 **/
 
 -- Update Version
