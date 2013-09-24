9bd1f1dd465b1c8aa84f28df58ae88b90776052b
Roman <openvoip.co@gmail.com>
2013-09-24 06:32:21 -0700

    added missed field to sql file

--- DataBase/mysql-5.x/UPDATE-a2billing-v2.0.3-to-develop.sql
+++ DataBase/mysql-5.x/UPDATE-a2billing-v2.0.3-to-develop.sql
@@ -32,33 +32,34 @@
 **/
 
 alter table `cc_trunk`
-	add column `minutes_per_day` int(11)   NULL DEFAULT '0' after `if_max_use`, 
-	add column `attempt_statuses` varchar(255)  COLLATE utf8_bin NULL DEFAULT 'CHANUNAVAIL,CONGESTION' after `minutes_per_day`, 
-	add column `attempt_condition` int(11) unsigned   NULL DEFAULT '0' after `attempt_statuses`, 
-	add column `attempt_count` int(11) unsigned   NULL DEFAULT '0' after `attempt_condition`, COMMENT='',
-	add column `priority` int(11)   NULL DEFAULT '0' after `attempt_count`, COMMENT=''
-        add column `attempt_delay` int(11) DEFAULT '0' after `priority`, COMMENT='';
+	add column `minutes_per_day` int(11)   NULL DEFAULT '0' after `if_max_use`,
+	add column `attempt_statuses` varchar(255)  COLLATE utf8_bin NULL DEFAULT 'CHANUNAVAIL,CONGESTION' after `minutes_per_day`,
+	add column `attempt_condition` int(11) unsigned   NULL DEFAULT '0' after `attempt_statuses`,
+	add column `attempt_count` int(11) unsigned   NULL DEFAULT '0' after `attempt_condition`,
+	add column `priority` int(11)   NULL DEFAULT '0' after `attempt_count`,
+        add column `attempt_delay` int(11) DEFAULT '0' after `priority`,
 
 create table `cc_trunk_counter`(
-	`id_trunk` int(10) unsigned NOT NULL   , 
-	`calldate` date NOT NULL   , 
-	`seconds` int(11) NULL  DEFAULT '0'  , 
-	PRIMARY KEY (`id_trunk`,`calldate`) 
+	`id_trunk` int(10) unsigned NOT NULL,
+	`calldate` date NOT NULL,
+	`seconds` int(11) NULL  DEFAULT '0',
+        `last_call_time` int(11) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id_trunk`,`calldate`)
 )Engine=MyISAM DEFAULT CHARSET='utf8';
 
 alter table `cc_provider`
-	add column `id_cc_card` int(11)   NOT NULL DEFAULT '0' after `description`, COMMENT='';
+	add column `id_cc_card` int(11)   NOT NULL DEFAULT '0' after `description`;
 
 alter table `cc_ratecard`
-	add column `is_disabled` int(1) unsigned   NOT NULL DEFAULT '0' after `destination`, COMMENT='';
+	add column `is_disabled` int(1) unsigned   NOT NULL DEFAULT '0' after `destination`;
 
 alter table `cc_tariffplan` 
-	add column `trunk_algo` int(10) unsigned   NULL DEFAULT '0' after `calleridprefix`, COMMENT='';
+	add column `trunk_algo` int(10) unsigned   NULL DEFAULT '0' after `calleridprefix`;
 
 create table `cc_tariffplan_trunk`(
-	`idtariffplan` int(11) NOT NULL, 
-	`idtrunk` int(11) NOT NULL, 
-	PRIMARY KEY (`idtariffplan`,`idtrunk`) 
+	`idtariffplan` int(11) NOT NULL,
+	`idtrunk` int(11) NOT NULL,
+	PRIMARY KEY (`idtariffplan`,`idtrunk`)
 )Engine=MyISAM DEFAULT CHARSET='utf8';
 
 insert into cc_config
