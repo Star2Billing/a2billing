8ddd19732294c5b215d7e2f919cafc4064be4087
areski <areski@gmail.com>
2014-04-10 18:55:10 +0200

    clean up code

--- AGI/a2billing.php
+++ AGI/a2billing.php
@@ -643,7 +643,8 @@ if ($mode == 'standard') {
             " aleg_carrier_connect_charge_offp, aleg_carrier_cost_min_offp, aleg_retail_connect_charge_offp, aleg_retail_cost_min_offp, ".
             " aleg_carrier_initblock_offp, aleg_carrier_increment_offp, aleg_retail_initblock_offp, aleg_retail_increment_offp ".
             " FROM cc_did, cc_did_destination, cc_card ".
-            " WHERE id_cc_did=cc_did.id and cc_card.status=1 AND cc_card.id=id_cc_card AND cc_did_destination.activated=1 and cc_did.activated=1 AND did='$mydnid' ".
+            " WHERE id_cc_did=cc_did.id AND cc_card.status=1 AND cc_card.id=id_cc_card AND cc_did_destination.activated=1 ".
+            " AND cc_did.activated=1 AND did='$mydnid' ".
             " AND cc_did.startingdate<= CURRENT_TIMESTAMP AND (cc_did.expirationdate > CURRENT_TIMESTAMP OR cc_did.expirationdate IS NULL ".
             " AND cc_did_destination.validated=1";
         if ($A2B->config["database"]['dbtype'] != "postgres") {
