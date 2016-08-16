18392c8f62889077c246a2ec55f61580109a83ed
Roman Davydov <openvoip.co@gmail.com>
2016-08-15 22:39:35 -0400

    another fix wtf

--- addons/contrib/openvoip.co/cdr_upload.sh
+++ addons/contrib/openvoip.co/cdr_upload.sh
@@ -4,8 +4,13 @@ SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P );
 cdr_log=/var/log/asterisk/cdr-csv/Master.csv
 cdr_log_tmp=/var/log/asterisk/cdr-csv/Master.bak
 
-[ ! -f $cdr_log -o "$(stat -c%s $cdr_log)" == "0" ] && {
+[ ! -f $cdr_log ] && {
     echo "No cdrs in $cdr_log"
+    exit 1
+}
+
+[ "$(stat -c%s $cdr_log)" == "0" ] && {
+    echo "Empty cdr file $cdr_log"
     exit 1
 }
 
