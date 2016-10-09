7975dae8367717a5bb5fc8a5f42ea49fd777d98c
Roman Davydov <openvoip.co@gmail.com>
2016-10-09 00:08:03 -0400

    suffix removed

--- addons/contrib/openvoip.co/cdr_upload.sh
+++ addons/contrib/openvoip.co/cdr_upload.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P );
-SUFFIX=""
-ARGS="$@"
-
-# get cmd options
-eval set -- `getopt -o s: --quiet -- "$@"`
-while true; do
-    case $1 in
-        -s)
-            SUFFIX="$2"
-            shift 2 ;;
-        --)
-            shift ;;
-        *)
-            break ;;
-    esac
-done
 
 # run parser and redirect output to NMS server
-php $SCRIPT_DIR/cdr_format_mysql.php $ARGS | ssh cdranalyst@nms "cat >> /home/cdranalyst/cdrs/A$(date +%m%d%Y)$SUFFIX.txt"
+php $SCRIPT_DIR/cdr_format_mysql.php $ARGS | ssh cdranalyst@nms "cat >> /home/cdranalyst/cdrs/A$(date +%m%d%Y).txt"
