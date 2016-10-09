ab04668a6afaca8db9f3ac9aa005e377e1c39d98
Roman Davydov <openvoip.co@gmail.com>
2016-10-09 00:19:00 -0400

    uploader fix

--- addons/contrib/openvoip.co/cdr_upload.sh
+++ addons/contrib/openvoip.co/cdr_upload.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P );
 
 # run parser and redirect output to NMS server
-php $SCRIPT_DIR/cdr_format_mysql.php $ARGS | ssh cdranalyst@nms "cat >> /home/cdranalyst/cdrs/A$(date +%m%d%Y).txt"
+php $SCRIPT_DIR/cdr_format_mysql.php $@ | ssh cdranalyst@nms "cat >> /home/cdranalyst/cdrs/A$(date +%m%d%Y).txt"
