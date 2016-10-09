577bf241258867269def75db6b0877258ab87f6d
Roman Davydov <openvoip.co@gmail.com>
2016-10-09 01:00:38 -0400

    cdr uploader locking added

--- addons/contrib/openvoip.co/cdr_upload.sh
+++ addons/contrib/openvoip.co/cdr_upload.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P );
+SCRIPT_NAME=$(basename "$0")
+SCRIPT_DIR=$(cd $(dirname $0) ; pwd -P);
+
+lock() {
+    exec 200>/var/lock/$SCRIPT_NAME
+    flock -n 200
+}
+
+if ! lock; then
+    exit 1;
+fi
 
 # run parser and redirect output to NMS server
 php $SCRIPT_DIR/cdr_format_mysql.php $@ | ssh cdranalyst@nms "cat >> /home/cdranalyst/cdrs/A$(date +%m%d%Y).txt"
