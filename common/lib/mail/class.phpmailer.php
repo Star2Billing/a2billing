8d3013f494db6704af996f7d5e80392bbcd6f094
J.Cattan <jcattan@users.noreply.github.com>
2020-07-17 10:38:00 +0200

    Update class.phpmailer.php
    PHP Deprecated:  preg_replace(): The /e modifier is deprecated, use preg_replace_callback instead

--- common/lib/mail/class.phpmailer.php
+++ common/lib/mail/class.phpmailer.php
@@ -1673,8 +1673,12 @@ class phpmailer
       default:
         // Replace every high ascii, control =, ? and _ characters
         //TODO using /e (equivalent to eval()) is probably not a good idea
-        $encoded = preg_replace('/([\000-\011\013\014\016-\037\075\077\137\177-\377])/e',
-              "'='.sprintf('%02X', ord('\\1'))", $encoded);
+        //$encoded = preg_replace('/([\000-\011\013\014\016-\037\075\077\137\177-\377])/e',
+        //      "'='.sprintf('%02X', ord('\\1'))", $encoded);
+    $encoded = preg_replace_callback('/([\000-\011\013\014\016-\037\075\077\137\177-\377])/', function ($m) {
+		return "'='.sprintf('%02X', ord($m[1]))";
+		}, $encoded);     
+        
         break;
     }
 
